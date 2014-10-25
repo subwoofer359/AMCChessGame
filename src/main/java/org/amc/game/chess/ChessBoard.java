cd03cf97fd1ed917aafed78c4f17314956e62b8f
Adrian McLaughlin <subwoofer359@gmail.com>
2014-10-25 19:58:02 +0100

    CG-1 #comment create enum coordinate for the letter component of a move, create array 8x8 to store the location of pieces on the board

--- src/main/java/org/amc/game/chess/ChessBoard.java
+++ src/main/java/org/amc/game/chess/ChessBoard.java
@@ -1,6 +1,36 @@
 package org.amc.game.chess;
 
+
+/**
+ * Represents a Chess Board
+ * Responsibility is to know the position of all the pieces
+ * 
+ * @author Adrian Mclaughlin
+ *
+ */
 public class ChessBoard
 {
-
+	public enum Coordinate{
+		A(0),
+		B(1),
+		C(2),
+		D(3),
+		E(4),
+		F(5),
+		G(6),
+		H(7);
+		
+		private int name;
+		
+		private Coordinate(int name){
+			this.name=name;
+		}
+		
+		public int getName(){
+			return this.name;
+		}
+		
+	}
+	
+	private final ChessPiece[][] board=new ChessPiece[8][8];
 }
