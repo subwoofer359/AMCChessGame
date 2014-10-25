d90059a20409c9627f42aa69c55fa4e7589d4a6e
Adrian McLaughlin <subwoofer359@gmail.com>
2014-10-25 19:57:00 +0100

    CG-1 #comment add method isValidMove to ChessPiece interface

--- src/main/java/org/amc/game/chess/ChessPiece.java
+++ src/main/java/org/amc/game/chess/ChessPiece.java
@@ -1,6 +1,13 @@
 package org.amc.game.chess;
 
+/**
+ * Represents a Chess piece.
+ * Responsible for the piece's valid moves
+ * 
+ * @author Adrian McLaughlin
+ *
+ */
 public interface ChessPiece
 {
-
+	public boolean isValidMove();
 }
