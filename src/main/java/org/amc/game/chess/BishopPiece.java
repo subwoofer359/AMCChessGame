1d90b485887bdadeebe1afbecf0bb12122cd0463
Adrian McLaughlin <subwoofer359@gmail.com>
2014-10-28 13:18:33 +0000

    CS-1 #time 4m #comment Create a bishop chess piece class

---
+++ src/main/java/org/amc/game/chess/BishopPiece.java
@@ -0,0 +1,26 @@
+package org.amc.game.chess;
+
+import org.amc.game.chess.ChessBoard.Coordinate;
+/**
+ * Represents a Bishop in the game of chess
+ * @author Adrian Mclaughlin
+ *
+ */
+public class BishopPiece implements ChessPiece
+{
+	private Colour colour;
+	
+	public BishopPiece(Colour colour){
+		this.colour=colour;
+	}
+	
+	@Override
+	public boolean isValidMove(Coordinate letterCoordinate, int numberCoordinate)
+	{
+		return false;
+	}
+	
+	public Colour getColour(){
+		return this.colour;
+	}
+}
