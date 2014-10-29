4c28a3fe21ca6ab5cae4c992008eb1af6f64da0f
Adrian McLaughlin <subwoofer359@gmail.com>
2014-10-29 15:46:22 +0000

    CG-3 Add King ChessPiece

--- src/main/java/org/amc/game/chess/KingPiece.java
+++ src/main/java/org/amc/game/chess/KingPiece.java
@@ -10,6 +10,34 @@ public class KingPiece extends SimplePiece
 	@Override
 	public boolean isValidMove(ChessBoard board, Move move)
 	{
-		return false;
+		if(validMovement(move.getAbsoluteDistanceX(),move.getAbsoluteDistanceY())){
+			return canMakeMove(board, move);
+		}else{
+			return false;
+		}
+	}
+	
+	/**
+	 * King can only move one space in any direction
+	 * @param distanceX
+	 * @param distanceY
+	 * @return if true if a valid move
+	 */
+	private boolean validMovement(int distanceX,int distanceY){
+		return (distanceX<=1 && distanceY<=1);
+	}
+	
+	private boolean canMakeMove(ChessBoard board,Move move){
+		ChessPiece piece=board.getPieceFromBoardAt(
+				move.getEnd().getLetter().getName(), move.getEnd().getNumber());
+		if(piece==null){
+			return true;
+		}else{
+			return endSquareNotOccupiedByPlayersOwnPiece(piece);
+		}
+	}
+	
+	private boolean endSquareNotOccupiedByPlayersOwnPiece(ChessPiece piece){
+		return !piece.getColour().equals(getColour());
 	}
 }
---
+++ src/test/java/org/amc/game/chess/KingPieceTest.java
@@ -0,0 +1,136 @@
+package org.amc.game.chess;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class KingPieceTest
+{
+	private ChessBoard board;
+	
+	@Before
+	public void setUp() throws Exception
+	{
+		board=new ChessBoard();
+		board.initialise();
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+	}
+
+	@Test
+	public void testIsValidMoveOnEmptyBoard()
+	{
+		KingPiece king=new KingPiece(Colour.BLACK);
+		board.putPieceOnBoardAt(king,new Location(ChessBoard.Coordinate.F, 7));
+		assertTrue(king.isValidMove(board, new Move(
+				new Location(ChessBoard.Coordinate.F,7),
+				new Location(ChessBoard.Coordinate.F,8)
+				)));
+		assertTrue(king.isValidMove(board, new Move(
+				new Location(ChessBoard.Coordinate.F,7),
+				new Location(ChessBoard.Coordinate.F,6)
+				)));
+		assertTrue(king.isValidMove(board, new Move(
+				new Location(ChessBoard.Coordinate.F,7),
+				new Location(ChessBoard.Coordinate.G,7)
+				)));
+		assertTrue(king.isValidMove(board, new Move(
+				new Location(ChessBoard.Coordinate.F,7),
+				new Location(ChessBoard.Coordinate.E,7)
+				)));
+		
+		assertTrue(king.isValidMove(board, new Move(
+				new Location(ChessBoard.Coordinate.F,7),
+				new Location(ChessBoard.Coordinate.E,8)
+				)));
+		assertTrue(king.isValidMove(board, new Move(
+				new Location(ChessBoard.Coordinate.F,7),
+				new Location(ChessBoard.Coordinate.G,8)
+				)));
+		assertTrue(king.isValidMove(board, new Move(
+				new Location(ChessBoard.Coordinate.F,7),
+				new Location(ChessBoard.Coordinate.G,6)
+				)));
+		assertTrue(king.isValidMove(board, new Move(
+				new Location(ChessBoard.Coordinate.F,7),
+				new Location(ChessBoard.Coordinate.E,8)
+				)));
+	}
+	
+	@Test
+	public void testIsNotValidMoveOnEmptyBoard()
+	{
+		KingPiece king=new KingPiece(Colour.BLACK);
+		board.putPieceOnBoardAt(king,new Location(ChessBoard.Coordinate.F, 7));
+		assertFalse(king.isValidMove(board, new Move(
+				new Location(ChessBoard.Coordinate.F,7),
+				new Location(ChessBoard.Coordinate.A,8)
+				)));
+		assertFalse(king.isValidMove(board, new Move(
+				new Location(ChessBoard.Coordinate.F,7),
+				new Location(ChessBoard.Coordinate.B,6)
+				)));
+		assertFalse(king.isValidMove(board, new Move(
+				new Location(ChessBoard.Coordinate.F,7),
+				new Location(ChessBoard.Coordinate.G,3)
+				)));
+		assertFalse(king.isValidMove(board, new Move(
+				new Location(ChessBoard.Coordinate.F,7),
+				new Location(ChessBoard.Coordinate.E,1)
+				)));
+		
+		assertFalse(king.isValidMove(board, new Move(
+				new Location(ChessBoard.Coordinate.F,7),
+				new Location(ChessBoard.Coordinate.D,2)
+				)));
+		assertFalse(king.isValidMove(board, new Move(
+				new Location(ChessBoard.Coordinate.F,7),
+				new Location(ChessBoard.Coordinate.C,3)
+				)));
+		assertFalse(king.isValidMove(board, new Move(
+				new Location(ChessBoard.Coordinate.F,7),
+				new Location(ChessBoard.Coordinate.G,3)
+				)));
+		assertFalse(king.isValidMove(board, new Move(
+				new Location(ChessBoard.Coordinate.F,7),
+				new Location(ChessBoard.Coordinate.D,8)
+				)));
+		
+		assertFalse(king.isValidMove(board, new Move(
+				new Location(ChessBoard.Coordinate.F,7),
+				new Location(ChessBoard.Coordinate.F,7)
+				)));
+		
+		
+	}
+	
+	@Test
+	public void testIsValidMoveOnBoard()
+	{
+		KingPiece king=new KingPiece(Colour.BLACK);
+		KingPiece kingWhite=new KingPiece(Colour.WHITE);
+		KingPiece anotherBlackKing=new KingPiece(Colour.BLACK);
+		board.putPieceOnBoardAt(king,new Location(ChessBoard.Coordinate.F, 7));
+		board.putPieceOnBoardAt(kingWhite,new Location(ChessBoard.Coordinate.E, 7));
+		board.putPieceOnBoardAt(anotherBlackKing,new Location(ChessBoard.Coordinate.G, 7));
+		
+		//Move to square occupied by the white king should be valid
+		boolean isValid =king.isValidMove(board, new Move(
+				new Location(ChessBoard.Coordinate.F,7),
+				new Location(ChessBoard.Coordinate.E,7)
+				));
+		//Move to square occupied by the another king should not be valid		
+		boolean notValid=king.isValidMove(board, new Move(
+				new Location(ChessBoard.Coordinate.F,7),
+				new Location(ChessBoard.Coordinate.G,7)
+				));
+
+		assertTrue(isValid);
+		assertFalse(notValid);
+	}
+}
