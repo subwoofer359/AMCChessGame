29904941c6ff0ac89992c50d7f1440f4758c2b0d
Adrian McLaughlin <subwoofer359@gmail.com>
2014-10-30 13:09:05 +0000

    Created Console version of the game

--- src/main/java/org/amc/game/chess/ChessBoardView.java
+++ src/main/java/org/amc/game/chess/ChessBoardView.java
@@ -4,6 +4,8 @@ import org.amc.game.chess.ChessBoard.Coordinate;
 import org.amc.util.Observer;
 import org.amc.util.Subject;
 
+import java.io.Console;
+
 
 /**
  * Creates a simple console base view of the ChessBoard
@@ -97,5 +99,49 @@ public class ChessBoardView implements Observer{
         return sb;
     }
 
+    public static void main(String[] args){
+        Console console=System.console();
+        if(console==null){
+            System.err.println("A Console couldn't be retrieved");
+            System.exit(1);
+        }
+        ChessBoard board=new ChessBoard();
+        board.initialise();
+        Player playerOne=new HumanPlayer("Stephen", Colour.WHITE);
+        Player playerTwo=new HumanPlayer("Chris", Colour.BLACK);
+        Player currentPlayer=playerOne;
+        
+        ChessBoardView view=new ChessBoardView(board);
+        view.displayTheBoard();
+        START:while(true){
+            String input=console.readLine("Player(%s) move:",currentPlayer.getName());
+            if(input.length()!=4){
+                System.out.println("Move must be enter like so: a1b2");
+            }else{
+                String startString=input.substring(0, 2);
+                String endString=input.substring(2,4);
+                System.out.println(startString);
+                System.out.println(endString);
+                
+                Location startSquare=new Location(Coordinate.valueOf(String.valueOf(startString.charAt(0))),
+                                Integer.parseInt(String.valueOf(startString.charAt(1))));
+                Location endSquare=new Location(Coordinate.valueOf(String.valueOf(endString.charAt(0))),
+                                Integer.parseInt(String.valueOf(endString.charAt(1))));
+                
+                try {
+                    board.move(currentPlayer, new Move(startSquare,endSquare));
+                } catch (InvalidMoveException e) {
+                    System.out.println(e.getMessage());
+                    continue START;
+                }
+            }
+            
+            if(currentPlayer.equals(playerOne)){
+                currentPlayer=playerTwo;
+            }else{
+                currentPlayer=playerOne;
+            }
+        }
+    }
     
 }
--- src/test/java/org/amc/game/chess/ChessBoardViewTest.java
+++ src/test/java/org/amc/game/chess/ChessBoardViewTest.java
@@ -23,5 +23,5 @@ public class ChessBoardViewTest {
         ChessBoardView view= new ChessBoardView(board);
         view.displayTheBoard();
     }
-
+ 
 }
