52ac042e2fd727e74940b328eed98d8632dc4eac
Adrian McLaughlin <subwoofer359@gmail.com>
2014-11-07 09:50:24 +0000

    CG-5 #comment create classes

---
+++ src/main/java/org/amc/game/chess/KnightPiece.java
@@ -0,0 +1,21 @@
+package org.amc.game.chess;
+
+public class KnightPiece implements ChessPiece {
+
+    public KnightPiece() {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public boolean isValidMove(ChessBoard board, Move move) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Colour getColour() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
