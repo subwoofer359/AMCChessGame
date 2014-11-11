5bc030058e110afc7db314cf46f4b1ca662dde35
Adrian McLaughlin <subwoofer359@gmail.com>
2014-11-11 16:09:29 +0000

    Create QueenPiece skeleton

---
+++ src/main/java/org/amc/game/chess/QueenPiece.java
@@ -0,0 +1,16 @@
+package org.amc.game.chess;
+
+public class QueenPiece extends ComplexPiece {
+
+    public QueenPiece(Colour colour) {
+        super(colour);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    boolean validMovement(Move move) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
---
+++ src/test/java/org/amc/game/chess/QueenPieceTest.java
@@ -0,0 +1,54 @@
+package org.amc.game.chess;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class QueenPieceTest implements ChessPieceTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+    @Override
+    public void testOnEmptyBoardIsValidMove() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void testOnEmptyBoardIsNotValidMove() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void testOnBoardIsValidCapture() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void testOnBoardInvalidCapture() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void testOnBoardIsNotValidMove() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
