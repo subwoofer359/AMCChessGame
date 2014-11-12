9aab93e7e25fdb4ca0c7f41691ddb7708400171b
Adrian McLaughlin <subwoofer359@gmail.com>
2014-11-12 11:50:35 +0000

    CG-6 #status IN PROGRESS  #comment implement 3 test cases
    Test cases implemented
    
    testOnEmptyBoardIsValidMove,
    testOnEmptyBoardIsNotValidMove,
    testOnEmptyBoardIsNotValidNonIntialMove

--- src/main/java/org/amc/game/chess/PawnPiece.java
+++ src/main/java/org/amc/game/chess/PawnPiece.java
@@ -2,18 +2,54 @@ package org.amc.game.chess;
 
 public class PawnPiece extends SimplePiece {
 
+    private Boolean initialMove=true;
     public PawnPiece(Colour colour) {
         super(colour);
     }
 
     @Override
     boolean validMovement(Move move) {
-        return false;
+        if(move.getAbsoluteDistanceX()==0){
+            if(initialMove){
+                return isMovingForwardOneOrTwoSquares(move);
+            }else{
+                return isMovingForwardOneSquareOnly(move);
+            }
+        }
+        else{
+            return isMovingForwardDiagonallyOneSquare(move);
+        }
     }
 
     @Override
     boolean canMakeMove(ChessBoard board, Move move) {
-        return false;
+        return true;
     }
 
+    public void moved(){
+        this.initialMove=false;
+    }
+    
+    private boolean isMovingForwardOneOrTwoSquares(Move move){
+        return isMovingForward(move) && move.getAbsoluteDistanceY()>0 && move.getAbsoluteDistanceY()<=2;
+    }
+    
+    private boolean isMovingForwardOneSquareOnly(Move move){
+        return isMovingForward(move) && move.getAbsoluteDistanceY()==1;
+    }
+    
+    private boolean isMovingForwardDiagonallyOneSquare(Move move){
+        return isMovingForward(move) && Move.isDiagonalMove(move) && move.getAbsoluteDistanceX()==1;
+    }
+    
+    private boolean isMovingForward(Move move){
+        int moveInYDirection=move.getDistanceY();
+        if(this.getColour().equals(Colour.WHITE)){
+            return moveInYDirection>0;
+        }
+        else{
+            return moveInYDirection<0;
+        }
+    }
+    
 }
--- src/test/java/org/amc/game/chess/PawnPieceTest.java
+++ src/test/java/org/amc/game/chess/PawnPieceTest.java
@@ -2,13 +2,26 @@ package org.amc.game.chess;
 
 import static org.junit.Assert.*;
 
+import org.amc.game.chess.ChessBoard.Coordinate;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class PawnPieceTest implements ChessPieceTest  {
     private ChessBoard board;
-    private Location testStartPosition = new Location(ChessBoard.Coordinate.D, 5);
+    private Location testStartPosition = new Location(ChessBoard.Coordinate.F, 2);
+    private static final Location[] invalidMovesFromF2={
+        new Location(ChessBoard.Coordinate.F,1),
+        new Location(ChessBoard.Coordinate.E,1),
+        new Location(ChessBoard.Coordinate.E,2),
+        new Location(ChessBoard.Coordinate.G,2),
+        new Location(ChessBoard.Coordinate.G,1),
+        new Location(ChessBoard.Coordinate.E,4),
+        new Location(ChessBoard.Coordinate.G,4),
+        new Location(ChessBoard.Coordinate.F,5),
+        new Location(ChessBoard.Coordinate.D,4),
+        new Location(ChessBoard.Coordinate.H,4),
+    };
     
     @Before
     public void setUp() throws Exception {
@@ -19,18 +32,42 @@ public class PawnPieceTest implements ChessPieceTest  {
     public void tearDown() throws Exception {
     }
 
+    @Test
     @Override
     public void testOnEmptyBoardIsValidMove() {
-
+        PawnPiece pawn=new PawnPiece(Colour.BLACK);
+        board.putPieceOnBoardAt(pawn, testStartPosition);
+        
+        assertTrue(pawn.isValidMove(board, new Move(testStartPosition,
+                        new Location(ChessBoard.Coordinate.F,3))));
+        assertTrue(pawn.isValidMove(board, new Move(testStartPosition,
+                        new Location(ChessBoard.Coordinate.F,4))));
         
     }
 
+    @Test
     @Override
     public void testOnEmptyBoardIsNotValidMove() {
-
+        PawnPiece pawn=new PawnPiece(Colour.BLACK);
+        board.putPieceOnBoardAt(pawn, testStartPosition);
+        
+        for(Location endLocation:invalidMovesFromF2){
+            assertFalse(pawn.isValidMove(board, new Move(testStartPosition,endLocation)));
+        }
         
     }
 
+    @Test
+    public void testOnEmptyBoardIsNotValidNonIntialMove() {
+        PawnPiece pawn=new PawnPiece(Colour.BLACK);
+        pawn.moved();
+        board.putPieceOnBoardAt(pawn, testStartPosition);
+        Location endLocation=new Location(Coordinate.F,4);
+        assertFalse(pawn.isValidMove(board, new Move(testStartPosition,endLocation)));
+        
+        
+    }
+    
     @Override
     public void testOnBoardIsValidCapture() {
 
