5ca0691f743b56410e9d7f73ede27f55b433fdaa
Adrian McLaughlin <subwoofer359@gmail.com>
2014-11-13 15:24:25 +0000

    refactors

--- src/test/java/org/amc/game/chess/ChessBoardTest.java
+++ src/test/java/org/amc/game/chess/ChessBoardTest.java
@@ -4,88 +4,68 @@ import static org.junit.Assert.assertEquals;
 
 import static org.junit.Assert.*;
 
-import org.amc.game.chess.ChessBoard.Coordinate;
-import org.amc.game.chess.TestChessGame.MockUserInput;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
-
-public class ChessBoardTest
-{
+public class ChessBoardTest {
 
     private ChessBoard board;
     private Player whitePlayer;
     private Player blackPlayer;
     private Location endLocation;
     private Location startLocation;
-	@Before
-	public void setUp() throws Exception
-	{
-	    board=new ChessBoard();
-	    whitePlayer=new HumanPlayer("Teddy", Colour.WHITE);
-	    blackPlayer=new HumanPlayer("Robin", Colour.BLACK);
-	    startLocation=new Location(ChessBoard.Coordinate.A,8);
-        endLocation=new Location(ChessBoard.Coordinate.B,7);
-	}
 
-	@After
-	public void tearDown() throws Exception
-	{
-	}
-
-	@Ignore
-	public void testInitialise()
-	{
-		fail("Not yet implemented");
-	}
-
-	@Test(expected=InvalidMoveException.class)
-	public void testPlayerCantMoveOtherPlayersPiece() throws InvalidMoveException
-	{
-	    BishopPiece bishop=new BishopPiece(Colour.WHITE);
-	    board.putPieceOnBoardAt(bishop, new Location(ChessBoard.Coordinate.A,8));
-	    
-	    board.move(blackPlayer, new Move(
-	                    new Location(ChessBoard.Coordinate.A,8),
-	                    new Location(ChessBoard.Coordinate.B,7)));
-	}
-	
-	@Test
-    public void testPlayerCanMoveTheirOwnPiece() throws InvalidMoveException
-    {
-        BishopPiece bishop=new BishopPiece(Colour.WHITE);
-        Location startofMove=new Location(ChessBoard.Coordinate.A,8);
-        Location endOfMove=new Location(ChessBoard.Coordinate.B,7);
-        
-        board.putPieceOnBoardAt(bishop,startofMove);
-        
-        board.move(whitePlayer, new Move(startofMove,endOfMove));
-        
-        assertEquals(bishop, board.getPieceFromBoardAt(endOfMove));
-        assertNull(board.getPieceFromBoardAt(startofMove));
+    @Before
+    public void setUp() throws Exception {
+        board = new ChessBoard();
+        whitePlayer = new HumanPlayer("Teddy", Colour.WHITE);
+        blackPlayer = new HumanPlayer("Robin", Colour.BLACK);
+        startLocation = new Location(ChessBoard.Coordinate.A, 8);
+        endLocation = new Location(ChessBoard.Coordinate.B, 7);
     }
-	
-	@Test(expected=InvalidMoveException.class)
-	public void testMoveWithAnEmptySquare()throws InvalidMoveException{
-	    Location startofMove=new Location(ChessBoard.Coordinate.A,8);
-        Location endOfMove=new Location(ChessBoard.Coordinate.B,7);
-        
-        
-        board.move(whitePlayer, new Move(startofMove,endOfMove));
-        
-	}
-	
-	   @Test
-	    public void testMovesAreSaved() throws InvalidMoveException{
-	        Player playerOne=new HumanPlayer("Stephen",Colour.BLACK);
-	        BishopPiece bishop=new BishopPiece(Colour.BLACK);
-	        board.putPieceOnBoardAt(bishop,startLocation);
-	        Move move =new Move(startLocation,endLocation);
-	        board.move(playerOne, move);
-	        Move lastMove=board.getTheLastMove();
-	        assertEquals(lastMove.getStart(),startLocation);
-	        assertEquals(lastMove.getEnd(),endLocation);
-	    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Ignore
+    public void testInitialise() {
+        fail("Not yet implemented");
+    }
+
+    @Test(expected = InvalidMoveException.class)
+    public void testPlayerCantMoveOtherPlayersPiece() throws InvalidMoveException {
+        BishopPiece bishop = new BishopPiece(Colour.WHITE);
+        board.putPieceOnBoardAt(bishop, startLocation);
+        board.move(blackPlayer, new Move(startLocation, new Location(
+                        ChessBoard.Coordinate.B, 7)));
+    }
+
+    @Test
+    public void testPlayerCanMoveTheirOwnPiece() throws InvalidMoveException {
+        BishopPiece bishop = new BishopPiece(Colour.WHITE);
+        board.putPieceOnBoardAt(bishop, startLocation);
+        board.move(whitePlayer, new Move(startLocation, endLocation));
+        assertEquals(bishop, board.getPieceFromBoardAt(endLocation));
+        assertNull(board.getPieceFromBoardAt(startLocation));
+    }
+
+    @Test(expected = InvalidMoveException.class)
+    public void testMoveWithAnEmptySquare() throws InvalidMoveException {
+        board.move(whitePlayer, new Move(startLocation, endLocation));
+    }
+
+    @Test
+    public void testMovesAreSaved() throws InvalidMoveException {
+        Player playerOne = new HumanPlayer("Stephen", Colour.BLACK);
+        BishopPiece bishop = new BishopPiece(Colour.BLACK);
+        board.putPieceOnBoardAt(bishop, startLocation);
+        Move move = new Move(startLocation, endLocation);
+        board.move(playerOne, move);
+        Move lastMove = board.getTheLastMove();
+        assertEquals(lastMove.getStart(), startLocation);
+        assertEquals(lastMove.getEnd(), endLocation);
+    }
 }
