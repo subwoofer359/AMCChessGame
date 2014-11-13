38de844f0e4916d9a0b144825e834256ebe727f0
Adrian McLaughlin <subwoofer359@gmail.com>
2014-11-13 14:54:19 +0000

    create test case for EmptyMove

---
+++ src/test/java/org/amc/game/chess/EmptyMoveTest.java
@@ -0,0 +1,34 @@
+package org.amc.game.chess;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EmptyMoveTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        Move move=new EmptyMove();
+        assertEquals(move.getAbsoluteDistanceX().intValue(), 0);
+        assertEquals(move.getAbsoluteDistanceY().intValue(), 0);
+        assertEquals(move.getDistanceX().intValue(), 0);
+        assertEquals(move.getDistanceY().intValue(), 0);
+        assertEquals(move.getStart().getLetter(),ChessBoard.Coordinate.A);
+        assertEquals(move.getStart().getNumber().intValue(),1);
+        assertEquals(move.getEnd().getLetter(),ChessBoard.Coordinate.A);
+        assertEquals(move.getEnd().getNumber().intValue(),1);
+        System.out.println(move.toString());
+        
+    }
+
+}
