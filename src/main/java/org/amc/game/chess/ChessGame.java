3c4b0f7c7e4d221540258f94ad74c46ad67e3ff4
Adrian McLaughlin <subwoofer359@gmail.com>
2014-11-26 19:14:51 +0000

    CG-17 #comment adds PawnPromotion rule to ChessGame

--- src/main/java/org/amc/game/chess/ChessGame.java
+++ src/main/java/org/amc/game/chess/ChessGame.java
@@ -26,6 +26,7 @@ public class ChessGame {
         chessRules=new ArrayList<>();
         chessRules.add(new EnPassantRule());
         chessRules.add(new CastlingRule());
+        chessRules.add(new PawnPromotionRule());
     }
     
     public Player getCurrentPlayer() {
