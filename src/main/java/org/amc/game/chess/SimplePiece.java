c2727643ce7ad3249a94a807df70d3fdf9aa4aae
Adrian McLaughlin <subwoofer359@gmail.com>
2014-11-27 16:29:34 +0000

    move getPossibleMoveLocations(ChessBoard,Location) to SimplePiece

--- src/main/java/org/amc/game/chess/SimplePiece.java
+++ src/main/java/org/amc/game/chess/SimplePiece.java
@@ -1,5 +1,10 @@
 package org.amc.game.chess;
 
+import org.amc.game.chess.ChessBoard.Coordinate;
+
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Shared implementation between ChessPieces
  * 
@@ -90,5 +95,27 @@ abstract class SimplePiece implements ChessPiece {
         } else {
             return true;
         }
+    }
+    
+    /**
+     * Find all possible move locations. If the piece isn't on the board the method will 
+     * add the @param Location to the Set which isn't desirable
+     * 
+     * @param board ChessBoard on which the ChessPiece is on
+     * @param location Location of the ChessPiece
+     * @return HashSet of Locations
+     */
+    public Set<Location> getPossibleMoveLocations(ChessBoard board,Location location){
+        Set<Location> locations=new HashSet<>();
+        for(Coordinate coord:Coordinate.values()){
+            for(int i=1;i<=board.BOARD_WIDTH;i++){
+                Location moveLocation=new Location(coord,i);
+                Move move=new Move(location,moveLocation);
+                if(isValidMove(board, move)){
+                    locations.add(moveLocation);
+                }
+            }
+        }
+        return locations;
     }
 }
