63f1a6128717f73a035986c416d1018aed5c3dbc
Adrian McLaughlin <subwoofer359@gmail.com>
2014-11-27 11:41:09 +0000

    add override equals and hashCode for Location

--- src/main/java/org/amc/game/chess/Location.java
+++ src/main/java/org/amc/game/chess/Location.java
@@ -49,6 +49,36 @@ public class Location implements Comparable<Location> {
     public int compareTo(Location o) {
         return this.getLetter().compareTo(o.getLetter());
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((letter == null) ? 0 : letter.hashCode());
+        result = prime * result + ((number == null) ? 0 : number.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Location other = (Location) obj;
+        if (letter != other.letter)
+            return false;
+        if (number == null) {
+            if (other.number != null)
+                return false;
+        } else if (!number.equals(other.number))
+            return false;
+        return true;
+    }
+    
+    
     
     
 }
