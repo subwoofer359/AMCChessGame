7b68036001d0ab6b7f799abff435de9f03534e95
Adrian McLaughlin <subwoofer359@gmail.com>
2014-11-28 13:33:13 +0000

    Remove unused constructors in InvalidMoveException

--- src/main/java/org/amc/game/chess/InvalidMoveException.java
+++ src/main/java/org/amc/game/chess/InvalidMoveException.java
@@ -10,10 +10,6 @@ package org.amc.game.chess;
 public class InvalidMoveException extends Exception {
     private static final long serialVersionUID = 1L;
 
-    public InvalidMoveException() {
-        super();
-    }
-
     public InvalidMoveException(String message) {
         super(message);
 
@@ -22,16 +18,6 @@ public class InvalidMoveException extends Exception {
     public InvalidMoveException(Throwable cause) {
         super(cause);
 
-    }
-
-    public InvalidMoveException(String message, Throwable cause) {
-        super(message, cause);
-
-    }
-
-    public InvalidMoveException(String message, Throwable cause, boolean enableSuppression,
-                    boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 
 }
