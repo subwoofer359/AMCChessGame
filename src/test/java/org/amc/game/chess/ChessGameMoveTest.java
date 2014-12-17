6d9a767391f0b004a427d71e8642ad21616f8170
Adrian McLaughlin <subwoofer359@gmail.com>
2014-12-17 09:41:28 +0000

    clears test cases
    need to redesign test cases

--- src/test/java/org/amc/game/chess/ChessGameMoveTest.java
+++ src/test/java/org/amc/game/chess/ChessGameMoveTest.java
@@ -5,11 +5,13 @@ import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Ignore
 public class ChessGameMoveTest {
     private Player whitePlayer;
     private Player blackPlayer;
