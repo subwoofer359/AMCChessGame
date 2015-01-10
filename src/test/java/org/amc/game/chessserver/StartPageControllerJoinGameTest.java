88d24e6a4c82d51ffeb780975a30275351c48071
Adrian McLaughlin <subwoofer359@gmail.com>
2015-01-10 19:18:05 +0000

    implement test cases for normal join, player joining own game and player joining game already in progress

--- src/test/java/org/amc/game/chessserver/StartPageControllerJoinGameTest.java
+++ src/test/java/org/amc/game/chessserver/StartPageControllerJoinGameTest.java
@@ -2,8 +2,6 @@ package org.amc.game.chessserver;
 
 import static org.junit.Assert.*;
 
-import org.amc.game.chess.ChessBoard;
-import org.amc.game.chess.ChessGame;
 import org.amc.game.chess.Colour;
 import org.amc.game.chess.HumanPlayer;
 import org.amc.game.chess.Player;
@@ -11,17 +9,16 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.mock.web.MockServletContext;
-import org.springframework.ui.ExtendedModelMap;
-import org.springframework.ui.Model;
+import org.springframework.test.web.ModelAndViewAssert;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 public class StartPageControllerJoinGameTest {
     private MockServletContext servletContext;
-    private Model model;
-    private ConcurrentMap<Long, ChessGame> gameMap;
-    private StartPageController controller;
+    private ConcurrentMap<Long, ServerChessGame> gameMap;
+    private ServerJoinChessGameController controller;
     private Player whitePlayer;
     private Player blackPlayer;
     private long gameUUID=1234L;
@@ -29,14 +26,13 @@ public class StartPageControllerJoinGameTest {
     @Before
     public void setUp() throws Exception {
         servletContext=new MockServletContext();
-        model=new ExtendedModelMap();
         gameMap =new ConcurrentHashMap<>();
         servletContext.setAttribute(ServerConstants.GAMEMAP.toString(), gameMap);
-        controller=new StartPageController();
+        controller=new ServerJoinChessGameController();
         controller.setServletContext(servletContext);
         whitePlayer=new HumanPlayer("Ted", Colour.WHITE);
         blackPlayer=new HumanPlayer("Chris", Colour.WHITE);
-        ChessGame chessGame=new ChessGame(new ChessBoard(),whitePlayer,blackPlayer);
+        ServerChessGame chessGame=new ServerChessGame(whitePlayer);
         gameMap.put(gameUUID, chessGame);
     }
 
@@ -46,8 +42,31 @@ public class StartPageControllerJoinGameTest {
 
     @Test
     public void test() {
-        model.addAttribute(ServerConstants.PLAYER.toString(), whitePlayer);
-        controller.joinGame(blackPlayer, gameUUID);
+        ModelAndView mav=controller.joinGame(gameMap,blackPlayer, gameUUID);
+        ModelAndViewAssert.assertViewName(mav, "chessGamePortal");
+        ModelAndViewAssert.assertModelAttributeAvailable(mav, "GAME_UUID");
+        ServerChessGame chessGame=gameMap.get(gameUUID);
+        assertEquals(ServerChessGame.status.IN_PROGRESS,chessGame.getCurrentStatus());
+        assertEquals(chessGame.getOpponent(),blackPlayer);
+    }
+    
+    @Test
+    public void testPlayerJoinsOwnGame(){
+        ModelAndView mav=controller.joinGame(gameMap,whitePlayer, gameUUID);
+        ModelAndViewAssert.assertViewName(mav, "forward:/app/chessgame/chessapplication");
+        ModelAndViewAssert.assertModelAttributeAvailable(mav, "ERRORS");
+        ServerChessGame chessGame=gameMap.get(gameUUID);
+        assertEquals(ServerChessGame.status.AWAITING_PLAYER,chessGame.getCurrentStatus());
+    }
+    
+    @Test
+    public void testPlayerJoinsGameAlreadyInProgress(){
+        ServerChessGame chessGame=gameMap.get(gameUUID);
+        chessGame.addOpponent(blackPlayer);
+        assertNotEquals(ServerChessGame.status.AWAITING_PLAYER,chessGame.getCurrentStatus());
+        ModelAndView mav=controller.joinGame(gameMap,blackPlayer, gameUUID);
+        ModelAndViewAssert.assertViewName(mav, "forward:/app/chessgame/chessapplication");
+        ModelAndViewAssert.assertModelAttributeAvailable(mav, "ERRORS");
         
     }
 
