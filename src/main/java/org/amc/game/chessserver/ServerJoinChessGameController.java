af6933833ec487ea5b054f09077e5bc0435bffbc
Adrian McLaughlin <subwoofer359@gmail.com>
2015-01-10 19:49:35 +0000

    second draft of joinGame

--- src/main/java/org/amc/game/chessserver/ServerJoinChessGameController.java
+++ src/main/java/org/amc/game/chessserver/ServerJoinChessGameController.java
@@ -16,43 +16,59 @@ import java.util.concurrent.ConcurrentMap;
 import javax.servlet.ServletContext;
 
 @Controller
-@SessionAttributes({"GAME_UUID","PLAYER"})
+@SessionAttributes({ "GAME_UUID", "PLAYER" })
 @RequestMapping("/joinGame")
 public class ServerJoinChessGameController {
     private static final Logger logger = Logger.getLogger(ServerJoinChessGameController.class);
     private ServletContext context;
-    
-    @RequestMapping(method=RequestMethod.POST)
-    public ModelAndView joinGame(@ModelAttribute("GAMEMAP") ConcurrentMap<Long, ServerChessGame> gameMap, @ModelAttribute("PLAYER") Player player,@RequestParam long gameUUID){
-        ModelAndView mav=new ModelAndView();
-        
-        ServerChessGame chessGame=gameMap.get(gameUUID);
-        if(!chessGame.getCurrentStatus().equals(ServerChessGame.status.AWAITING_PLAYER)){
-            mav.setViewName("forward:/app/chessgame/chessapplication");
-            mav.getModel().put("ERRORS", "Can't join chess game");
-        }else if(player.equals(chessGame.getPlayer())){
-            mav.setViewName("forward:/app/chessgame/chessapplication");
-            mav.getModel().put("ERRORS", "Player can't join a game they started");
-        }else{
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ModelAndView joinGame(
+                    @ModelAttribute("GAMEMAP") ConcurrentMap<Long, ServerChessGame> gameMap,
+                    @ModelAttribute("PLAYER") Player player, @RequestParam long gameUUID) {
+        ServerChessGame chessGame = gameMap.get(gameUUID);
+        return addPlayerToGame(chessGame, player, gameUUID);
+    }
+
+    private ModelAndView addPlayerToGame(ServerChessGame chessGame, Player player, long gameUUID) {
+        ModelAndView mav = new ModelAndView();
+        if (notInAwaitingPlayerState(chessGame)) {
+            setErrorPageAndMessage(mav, "Can't join chess game");
+        } else if (isPlayerJoiningOwnGame(chessGame, player)) {
+            setErrorPageAndMessage(mav, "Player can't join a game they started");
+        } else {
             chessGame.addOpponent(player);
             mav.getModel().put("GAME_UUID", gameUUID);
-            logger.info(String.format("Chess Game(%d): has been started",gameUUID));
+            logger.info(String.format("Chess Game(%d): has been started", gameUUID));
             mav.setViewName("chessGamePortal");
         }
         return mav;
     }
 
-    @Autowired
-    void setServletContext(ServletContext context){
-        this.context=context;
+    private boolean notInAwaitingPlayerState(ServerChessGame chessGame) {
+        return !chessGame.getCurrentStatus().equals(ServerChessGame.status.AWAITING_PLAYER);
     }
-    
+
+    private boolean isPlayerJoiningOwnGame(ServerChessGame chessGame, Player player) {
+        return player.equals(chessGame.getPlayer());
+    }
+
+    private void setErrorPageAndMessage(ModelAndView mav, String errorMessage) {
+        mav.setViewName("forward:/app/chessgame/chessapplication");
+        mav.getModel().put("ERRORS", errorMessage);
+    }
+
+    @Autowired
+    void setServletContext(ServletContext context) {
+        this.context = context;
+    }
+
     @ModelAttribute("GAMEMAP")
     @SuppressWarnings(value = "unchecked")
     private ConcurrentMap<Long, ServerChessGame> getGameMap() {
         synchronized (context) {
-            return (ConcurrentMap<Long, ServerChessGame>) context.getAttribute(ServerConstants.GAMEMAP
-                            .toString());
+            return (ConcurrentMap<Long, ServerChessGame>) context
+                            .getAttribute(ServerConstants.GAMEMAP.toString());
         }
     }
 }
