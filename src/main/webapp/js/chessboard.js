fbfba5bdcc0e58f9d1a57627d1231329dfb614d7
Adrian McLaughlin <subwoofer359@gmail.com>
2015-03-17 16:21:57 +0000

    add bishop chesspieces - not working

--- src/main/webapp/js/chessboard.js
+++ src/main/webapp/js/chessboard.js
@@ -22,62 +22,91 @@ var blackSquareColour = '#000000';
  * @returns SVG element
  */
 function createBlankChessBoardSVG() {
-    var svgDocument = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    var svgNS = svgDocument.namespaceURI;
+    var svgDocument = document.createElementNS("http://www.w3.org/2000/svg", "svg"),
+        svgNS = svgDocument.namespaceURI,
+        layer = document.createElementNS(svgNS, 'g'),
+        x = 0,
+        y = 0,
+        whiteToggle = true,
+        i,
+        letter,
+        rect;
+
     svgDocument.setAttribute('height', '500px');
     svgDocument.setAttribute('width', '500px');
-
-    var layer = document.createElementNS(svgNS, 'g');
     layer.setAttribute('id', 'layer1');
 
-    var x = 0, y = 0, whiteToggle = true;
-    for (var i = boardWidth;i > 0; i--) {
-        x=0;
-        for (var letter in letterCoordinates) {
-            var rect=document.createElementNS(svgNS, 'rect');
-            rect.setAttribute('id', (letterCoordinates[letter]+i));
-            rect.setAttribute('x', x);
-            rect.setAttribute('y', y);
-            rect.setAttribute('width', 62.5);
-            rect.setAttribute('height', 62.5);
-            if(whiteToggle){
-                rect.setAttribute('fill', whiteSquareColour);
-            }else{
-                rect.setAttribute('fill', blackSquareColour);
+    for (i = boardWidth; i > 0; i--) {
+        x = 0;
+        for (letter in letterCoordinates) {
+            if (letterCoordinates.hasOwnProperty(letter)) {
+                rect = document.createElementNS(svgNS, 'rect');
+                rect.setAttribute('id', (letterCoordinates[letter] + i));
+                rect.setAttribute('x', x);
+                rect.setAttribute('y', y);
+                rect.setAttribute('width', 62.5);
+                rect.setAttribute('height', 62.5);
+                if (whiteToggle) {
+                    rect.setAttribute('fill', whiteSquareColour);
+                } else {
+                    rect.setAttribute('fill', blackSquareColour);
+                }
+                whiteToggle = !whiteToggle;
+                rect.setAttribute('stroke', 'none');
+                rect.setAttribute('fill-opacity', '1');
+                rect.setAttribute('class', 'dropzone');
+                layer.appendChild(rect);
+                x = x + 62.5;
             }
-            whiteToggle=!whiteToggle;
-            rect.setAttribute('stroke', 'none');
-            rect.setAttribute('fill-opacity', '1');
-            rect.setAttribute('class', 'dropzone');
-            layer.appendChild(rect);
-            x = x + 62.5;    
         }
         y = y + 62.5;
-        whiteToggle=!whiteToggle; //alternate black white pattern
-        
+        whiteToggle = !whiteToggle; //alternate black white pattern
+
     }
     svgDocument.appendChild(layer);
     return svgDocument;
 }
 
 function createChessBoard(chessboardJSON) {
-    var json = $.parseJSON(chessboardJSON);
+    var json = $.parseJSON(chessboardJSON),
+        board,
+        chessBoardSVG = $("#chessboard-surround"),
+        location;
     console.log("parsed JSON");
-    var chessBoardSVG=$("#chessboard-surround");
+
     chessBoardSVG.html(createBlankChessBoardSVG());
-    for (var location in json){
-        switch(json[location]){
-                case "p":
-                console.log("Create pawn");
-                $("#layer1").append(chesspieces.pawn("test-"+location,location,chesspieces.colour.white));
+    board = $("#layer1");
+    for (location in json.squares) {
+        if (json.squares.hasOwnProperty(location)) {
+            switch (json.squares[location]) {
+            case "p":
+                board.append(chesspieces.pawn("test-" + location, location, chesspieces.colour.white));
                 break;
-                case 'r':
-                console.log("Create rook");
-                $("#layer1").append(chesspieces.rook("test-"+location,location,chesspieces.colour.black));
+
+            case "P":
+                board.append(chesspieces.pawn("test-" + location, location, chesspieces.colour.black));
                 break;
+
+            case 'r':
+                board.append(chesspieces.rook("test-" + location, location, chesspieces.colour.white));
+                break;
+
+            case 'R':
+                board.append(chesspieces.rook("test-" + location, location, chesspieces.colour.black));
+                break;
+
+            case 'B':
+                board.append(chesspieces.bishop("test-" + location, location, chesspieces.colour.black));
+                break;
+
+            case 'b':
+                board.append(chesspieces.bishop("test-" + location, location, chesspieces.colour.white));
+                break;
+
+            }
         }
     }
-                
+    console.log(chessBoardSVG.html());
     chessBoardSVG.html(chessBoardSVG.html());
-    
+
 }
