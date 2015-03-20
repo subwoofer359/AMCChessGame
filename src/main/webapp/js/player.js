f1c2354b14e28e6e8785f69b8c5e432a2886f004
Adrian McLaughlin <subwoofer359@gmail.com>
2015-03-20 12:50:51 +0000

    CG-35 add player.js for player specific functions

---
+++ src/main/webapp/js/player.js
@@ -0,0 +1,22 @@
+function updatePlayer(message) {
+    "use strict";
+    var jsonPlayer, name, colour, whitePlayerName, whiteNameHolder, blackNameHolder;
+    jsonPlayer = $.parseJSON(message).currentPlayer;
+    name = jsonPlayer.name;
+    colour = jsonPlayer.colour;
+    console.log(name + " " + colour);
+
+    whitePlayerName = $("#white-player").find("span.name").text();
+    whiteNameHolder = $("#white-player").find("span.name");
+    blackNameHolder = $("#black-player").find("span.name");
+
+    console.log("White player's name is " + whitePlayerName);
+
+    if (whitePlayerName === name) {
+        whiteNameHolder.addClass("player-name-highlight");
+        blackNameHolder.removeClass("player-name-highlight");
+    } else {
+        whiteNameHolder.removeClass("player-name-highlight");
+        blackNameHolder.addClass("player-name-highlight");
+    }
+}
