b8b47a0f3b04e941410f9820c0dbb4f00affd712
Adrian McLaughlin <subwoofer359@gmail.com>
2015-04-15 14:45:04 +0100

    CG-51 add check for number and type of chesspieces on board

--- src/main/webapp/js/tests/chessboardTest.js
+++ src/main/webapp/js/tests/chessboardTest.js
@@ -1,3 +1,5 @@
+var json = '{"squares":{"C8":"B","A2":"p","A1":"r","C7":"P","A8":"R","A7":"P","E7":"P","E8":"K","G7":"P","G8":"N","E2":"p","E1":"k","G2":"p","G1":"n","C1":"b","C2":"p","D8":"Q","B2":"p","D7":"P","B8":"N","B7":"P","F8":"B","F7":"P","H8":"R","F1":"b","H7":"P","H2":"p","H1":"r","F2":"p","D2":"p","D1":"q","B1":"n"}}';
+
 QUnit.test("testing chessboard.js: creating a blank chess board", function (assert) {
     "use strict";
     var $board,
@@ -7,7 +9,6 @@ QUnit.test("testing chessboard.js: creating a blank chess board", function (asse
     $("#qunit-fixture").append(createBlankChessBoardSVG());
 
     $board = $("#qunit-fixture svg");
-    console.log($board);
     assert.equal($board.attr("width"), "500px");
     assert.equal($board.attr("height"), "500px");
 
@@ -27,11 +28,9 @@ QUnit.test("testing chessboard.js: creating a blank chess board", function (asse
 
 QUnit.test("testing chessboard.js: create a non blank chess board", function (assert) {
     "use strict";
-    var json = '{"squares":{"C8":"B","A2":"p","A1":"r","C7":"P","A8":"R","A7":"P","E7":"P","E8":"K","G7":"P","G8":"N","E2":"p","E1":"k","G2":"p","G1":"n","C1":"b","C2":"p","D8":"Q","B2":"p","D7":"P","B8":"N","B7":"P","F8":"B","F7":"P","H8":"R","F1":"b","H7":"P","H2":"p","H1":"r","F2":"p","D2":"p","D1":"q","B1":"n"}}',
-        $board,
+    var $board,
         $chesspieces;
-    
-    
+
     $("#qunit-fixture").append("<div id='chessboard-surround'></div>");
     createChessBoard("WHITE", json);
     $board = $("#chessboard-surround svg");
@@ -39,12 +38,25 @@ QUnit.test("testing chessboard.js: create a non blank chess board", function (as
     assert.equal($board.attr("height"), "500px");
     $chesspieces = $board.find("g.chesspiece");
     assert.equal($chesspieces.length, 32);
-    
+
     $chesspieces.each(function () {
         var idExpr = /(knight|queen|king|bishop|rook|pawn)\-[A-H][1-8]/;
         assert.ok(idExpr.test($(this).attr("id")));
     });
-    
-    
+});
+
+QUnit.test("testing chessboard.js: test no of chess pieces on chessboard", function (assert) {
+    "use strict";
+    var $board;
+
+    $("#qunit-fixture").append("<div id='chessboard-surround'></div>");
+    createChessBoard("WHITE", json);
+    $board = $("#chessboard-surround svg");
+    assert.equal($board.find('g[id^="pawn"]').length, 16);
+    assert.equal($board.find('g[id^="knight"]').length, 4);
+    assert.equal($board.find('g[id^="bishop"]').length, 4);
+    assert.equal($board.find('g[id^="rook"]').length, 4);
+    assert.equal($board.find('g[id^="queen"]').length, 2);
+    assert.equal($board.find('g[id^="king"]').length, 2);
 });
 
