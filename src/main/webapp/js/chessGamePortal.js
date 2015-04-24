28d981d31814d6007d28aa8c7f89d418cb32dfe4
Adrian McLaughlin <subwoofer359@gmail.com>
2015-04-24 12:16:02 +0100

    Separates Stomp code from actions on receipt of Stomp message

--- src/main/webapp/js/chessGamePortal.js
+++ src/main/webapp/js/chessGamePortal.js
@@ -5,96 +5,120 @@
 /*global console*/
 /*global Stomp*/
 
-function openStompConnection(websocketURL, gameUID, playerName, opponentName, playerColour) {
+function StompActions(gameUID, playerName, opponentName, playerColour) {
+    "use strict";
+    this.oldChessBoard = {};
+    this.alertBox = $("#my-alert");
+    this.alertBoxText = $("#my-alert .alert");
+    this.GAME_STATUS = {
+        WHITE_CHECKMATE : "WHITE_CHECKMATE",
+        BLACK_CHECKMATE : "BLACK_CHECKMATE",
+        STALEMATE : "STALEMATE",
+        WHITE_IN_CHECK : "WHITE_IN_CHECK",
+        BLACK_IN_CHECK : "BLACK_IN_CHECK"
+    };
+    this.gameUID = gameUID;
+    this.playerName = playerName;
+    this.opponentName = opponentName;
+    this.playerColour = playerColour;
+}
+
+StompActions.prototype = {
+    showFadingAlertMessage : function (message) {
+        "use strict";
+        this.alertBoxText.html(message);
+        this.alertBox.css("display", "block");
+        this.alertBox.css("opacity", "1");
+        this.alertBox.fadeOut(5000);
+    },
+
+    showAlertMessage : function (message) {
+        "use strict";
+        this.alertBoxText.html(message);
+        this.alertBox.css("display", "block");
+        this.alertBox.css("opacity", "1");
+    },
+
+    updateChessBoard : function (chessBoardJson) {
+        "use strict";
+        createChessBoard(this.playerColour, chessBoardJson);
+        this.oldChessBoard = chessBoardJson;
+        updatePlayer(chessBoardJson);
+    },
+
+    userUpdate : function (message) {
+        "use strict";
+        if (message.headers.TYPE === "ERROR") {
+            this.showFadingAlertMessage(message.body);
+            if (this.oldChessBoard !== undefined || !$.isEmptyObject(this.oldChessBoard)) {
+                createChessBoard(this.playerColour, this.oldChessBoard);
+            }
+        } else if (message.headers.TYPE === "UPDATE") {
+            this.updateChessBoard(message.body);
+        } else if (message.headers.TYPE === "INFO") {
+            console.log(message.body);
+        }
+    },
+    topicUpdate : function (message) {
+        "use strict";
+        if (message.headers.TYPE === "ERROR") {
+            this.updateChessBoard(message.body);
+        } else if (message.headers.TYPE === "STATUS") {
+            switch (message.body) {
+            case this.GAME_STATUS.WHITE_CHECKMATE:
+                this.showAlertMessage(this.opponentName + " has won the game");
+                break;
+            case this.GAME_STATUS.BLACK_CHECKMATE:
+                this.showAlertMessage(this.playerName + " has won the game");
+                break;
+            case this.GAME_STATUS.STALEMATE:
+                this.showAlertMessage("Game has ended in a draw");
+                break;
+            case this.GAME_STATUS.WHITE_IN_CHECK:
+                this.showFadingAlertMessage(this.playerName + "'s king is in check");
+                break;
+            case this.GAME_STATUS.BLACK_IN_CHECK:
+                this.showFadingAlertMessage(this.opponentName + "'s king is in check");
+                break;
+            default:
+                break;
+            }
+        } else if (message.headers.TYPE === "INFO") {
+            if (/has quit the game$/.test(message.body)) {
+                this.showAlertMessage(message.body);
+            } else {
+                console.log("INFO:" + message.body);
+            }
+        } else if (message.headers.TYPE === "UPDATE") {
+            this.updateChessBoard(message.body);
+        }
+    }
+};
+
+function openStompConnection(websocketURL, stompCallBack) {
     "use strict";
     var stompClient,
         socket;
 
+    if (!(typeof stompCallBack === 'function' && stompCallBack instanceof StompActions)) {
+        throw "callback function isn't an instance of StompActions";
+    }
     socket = new WebSocket(websocketURL);
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function () {
-        var oldChessBoard = {},
-            alertBox = $("#my-alert"),
-            alertBoxText = $("#my-alert .alert"),
-            GAME_STATUS = {
-                WHITE_CHECKMATE : "WHITE_CHECKMATE",
-                BLACK_CHECKMATE : "BLACK_CHECKMATE",
-                STALEMATE : "STALEMATE",
-                WHITE_IN_CHECK : "WHITE_IN_CHECK",
-                BLACK_IN_CHECK : "BLACK_IN_CHECK"
-            };
-
-        function showFadingAlertMessage(message) {
-            alertBoxText.html(message);
-            alertBox.css("display", "block");
-            alertBox.css("opacity", "1");
-            alertBox.fadeOut(5000);
-        }
-
-        function showAlertMessage(message) {
-            alertBoxText.html(message);
-            alertBox.css("display", "block");
-            alertBox.css("opacity", "1");
-        }
-
         $("#quit-btn").click(function () {
-            stompClient.send("/app/quit/" + gameUID, {priority: 9}, "quit");
+            stompClient.send("/app/quit/" + stompCallBack.gameUID, {priority: 9}, "quit");
         });
-
-        function updateChessBoard(playerColour, chessBoardJson) {
-            createChessBoard(playerColour, chessBoardJson);
-            oldChessBoard = chessBoardJson;
-            updatePlayer(chessBoardJson);
-        }
 
         stompClient.subscribe("/user/queue/updates", function (message) {
-            if (message.headers.TYPE === "ERROR") {
-                showFadingAlertMessage(message.body);
-                if (oldChessBoard !== undefined || !$.isEmptyObject(oldChessBoard)) {
-                    createChessBoard(playerColour, oldChessBoard);
-                }
-            } else if (message.headers.TYPE === "UPDATE") {
-                updateChessBoard(playerColour, message.body);
-            } else if (message.headers.TYPE === "INFO") {
-                console.log(message.body);
-            }
+            stompCallBack.userUpdate.call(stompCallBack, message);
         });
 
-        stompClient.subscribe("/topic/updates/" + gameUID, function (message) {
-            if (message.headers.TYPE === "ERROR") {
-                updateChessBoard(message.body);
-            } else if (message.headers.TYPE === "STATUS") {
-                switch (message.body) {
-                case GAME_STATUS.WHITE_CHECKMATE:
-                    showAlertMessage(opponentName + " has won the game");
-                    break;
-                case GAME_STATUS.BLACK_CHECKMATE:
-                    showAlertMessage(playerName + " has won the game");
-                    break;
-                case GAME_STATUS.STALEMATE:
-                    showAlertMessage("Game has ended in a draw");
-                    break;
-                case GAME_STATUS.WHITE_IN_CHECK:
-                    showFadingAlertMessage(playerName + "'s king is in check");
-                    break;
-                case GAME_STATUS.BLACK_IN_CHECK:
-                    showFadingAlertMessage(opponentName + "'s king is in check");
-                    break;
-                default:
-                    break;
-                }
-            } else if (message.headers.TYPE === "INFO") {
-                if (/has quit the game$/.test(message.body)) {
-                    showAlertMessage(message.body);
-                } else {
-                    console.log("INFO:" + message.body);
-                }
-            } else if (message.headers.TYPE === "UPDATE") {
-                updateChessBoard(playerColour, message.body);
-            }
+        stompClient.subscribe("/topic/updates/" + stompCallBack.gameUID, function (message) {
+            stompCallBack.topicUpdate.call(stompCallBack, message);
         });
 
-        stompClient.send("/app/get/" + gameUID, {priority: 9}, "Get ChessBoard");
+        stompClient.send("/app/get/" + stompCallBack.gameUID, {priority: 9}, "Get ChessBoard");
     });
     return stompClient;
 }
