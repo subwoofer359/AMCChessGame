87230650e5b9251793527e776185b986db5c2951
Adrian McLaughlin <subwoofer359@gmail.com>
2015-05-30 09:40:43 +0100

    format ServerChessGame

--- src/main/java/org/amc/game/chessserver/ServerChessGame.java
+++ src/main/java/org/amc/game/chessserver/ServerChessGame.java
@@ -11,7 +11,7 @@ import org.amc.game.chess.Player;
 import org.amc.game.chess.SetupChessBoard;
 import org.amc.util.DefaultSubject;
 import org.apache.log4j.Logger;
- 
+
 /**
  * Represents a ChessGame Application resident in a Spring container
  * 
@@ -19,19 +19,19 @@ import org.apache.log4j.Logger;
  * @version 1.0
  */
 public class ServerChessGame extends DefaultSubject {
-    public enum ServerGameStatus{
+    public enum ServerGameStatus {
         IN_PROGRESS,
         AWAITING_PLAYER,
         FINISHED
     }
-    
+
     private static final Logger logger = Logger.getLogger(ServerChessGame.class);
-    
-    ChessGame chessGame=null;
+
+    ChessGame chessGame = null;
     private ServerGameStatus currentStatus;
     private final ChessGamePlayer player;
     private ChessGamePlayer opponent;
-    
+
     /**
      * The unique identifier of this game
      */
@@ -40,33 +40,39 @@ public class ServerChessGame extends DefaultSubject {
     /**
      * Constructor
      * 
-     * @param uid long the unique identifier for this game
-     * @param player Player who created the game and will be the white player
+     * @param uid
+     *            long the unique identifier for this game
+     * @param player
+     *            Player who created the game and will be the white player
      */
     public ServerChessGame(long uid, Player player) {
         super();
         this.uid = uid;
-        this.player=new ChessGamePlayer(player,Colour.WHITE);
-        this.currentStatus=ServerGameStatus.AWAITING_PLAYER;
+        this.player = new ChessGamePlayer(player, Colour.WHITE);
+        this.currentStatus = ServerGameStatus.AWAITING_PLAYER;
     }
-    
+
     /**
      * Adds player to the black side of the chess game
-     * Only valid if ServerChessGame is in AWAITING_PLAYER state, no exception is thrown if not in that state
-     * @param opponent Player
+     * Only valid if ServerChessGame is in AWAITING_PLAYER state, no exception
+     * is thrown if not in that state
+     * 
+     * @param opponent
+     *            Player
      */
-    public void addOpponent(Player opponent){
+    public void addOpponent(Player opponent) {
         synchronized (this) {
-            if(this.currentStatus.equals(ServerGameStatus.AWAITING_PLAYER)){
-                if(ComparePlayers.comparePlayers(this.player, opponent)){
-                    logger.debug(String.format("Player:(%s) tried to join their own game", opponent.getName()));
+            if (this.currentStatus.equals(ServerGameStatus.AWAITING_PLAYER)) {
+                if (ComparePlayers.comparePlayers(this.player, opponent)) {
+                    logger.debug(String.format("Player:(%s) tried to join their own game",
+                                    opponent.getName()));
                 } else {
-                    
-                    this.opponent=new ChessGamePlayer(opponent,Colour.BLACK);
-                    ChessBoard board=new ChessBoard();
+
+                    this.opponent = new ChessGamePlayer(opponent, Colour.BLACK);
+                    ChessBoard board = new ChessBoard();
                     SetupChessBoard.setUpChessBoardToDefault(board);
-                    chessGame=new ChessGame(board,this.player,this.opponent);
-                    this.currentStatus=ServerGameStatus.IN_PROGRESS;
+                    chessGame = new ChessGame(board, this.player, this.opponent);
+                    this.currentStatus = ServerGameStatus.IN_PROGRESS;
                 }
             }
         }
@@ -74,6 +80,7 @@ public class ServerChessGame extends DefaultSubject {
 
     /**
      * Get current status of the ServerChessGame
+     * 
      * @return status enum
      */
     public final ServerGameStatus getCurrentStatus() {
@@ -84,23 +91,26 @@ public class ServerChessGame extends DefaultSubject {
 
     /**
      * Get the player who created the game
+     * 
      * @return Player
      */
     public final ChessGamePlayer getPlayer() {
         return player;
     }
-    
+
     /**
      * Return ChessGamePlayer with the same ID as Player
+     * 
      * @param player
      * @return ChessGamePlayer represents Player
      */
     public final ChessGamePlayer getPlayer(Player player) {
-        return ComparePlayers.comparePlayers(this.player, player)? this.player : this.opponent; 
+        return ComparePlayers.comparePlayers(this.player, player) ? this.player : this.opponent;
     }
-    
+
     /**
      * Set the ServerGame's status
+     * 
      * @param currentStatus
      */
     public final void setCurrentStatus(ServerGameStatus currentStatus) {
@@ -117,26 +127,29 @@ public class ServerChessGame extends DefaultSubject {
     public final ChessGame getChessGame() {
         return chessGame;
     }
-    
+
     /**
-     * @return Player opposing player 
+     * @return Player opposing player
      */
-    public final ChessGamePlayer getOpponent(){
+    public final ChessGamePlayer getOpponent() {
         return opponent;
     }
-    
+
     /**
      * Sends a move to the ChessGame
      * If successful the next player can take their turn
      * Updates the ServerChessGame's status
      * 
-     * @param player Player who's is making the move
-     * @param move Move to be made
-     * @throws IllegalMoveException if Move is illegal
+     * @param player
+     *            Player who's is making the move
+     * @param move
+     *            Move to be made
+     * @throws IllegalMoveException
+     *             if Move is illegal
      */
-    public final void move(ChessGamePlayer player,Move move) throws IllegalMoveException{
-        if(chessGame!=null){
-            synchronized(chessGame){
+    public final void move(ChessGamePlayer player, Move move) throws IllegalMoveException {
+        if (chessGame != null) {
+            synchronized (chessGame) {
                 chessGame.move(player, move);
                 chessGame.changePlayer();
             }
@@ -144,7 +157,7 @@ public class ServerChessGame extends DefaultSubject {
             checkGameStatus();
         }
     }
-    
+
     /**
      * Retrieve the Unique identifier of the {@link ServerChessGame}
      * 
@@ -162,23 +175,23 @@ public class ServerChessGame extends DefaultSubject {
         switch (chessGame.getGameState()) {
         case RUNNING:
             break;
-            
+
         case WHITE_CHECKMATE:
         case BLACK_CHECKMATE:
         case STALEMATE:
             setCurrentStatus(ServerGameStatus.FINISHED);
             break;
-            
+
         case BLACK_IN_CHECK:
         case WHITE_IN_CHECK:
             notifyObservers(chessGame.getGameState());
             break;
-            
+
         default:
             break;
         }
     }
-    
+
     /**
      * Remove Observers from receiving updates
      * Sets Status to FINISHED
