324490e4c87b94b62d440c6eb49c208ca4e2c5fd
Adrian McLaughlin <subwoofer359@gmail.com>
2015-06-04 09:42:23 +0100

    CG-80 remove EmailTransport and JavamailTransport

--- src/main/java/org/amc/game/chessserver/messaging/EmailTransport.java
+++
@@ -1,9 +0,0 @@
-package org.amc.game.chessserver.messaging;
-
-import javax.mail.Message;
-
-import javax.mail.MessagingException;
-
-public interface EmailTransport {
-    public void send(Message message) throws MessagingException;
-}
