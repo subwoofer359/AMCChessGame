5067437828d5799c225dc6c85390d91f26016d9b
Adrian McLaughlin <subwoofer359@gmail.com>
2015-06-18 20:36:09 +0100

    CG-80 refactor SVGChessPiece sub classes

--- src/main/java/org/amc/game/chessserver/messaging/svg/SVGBishopPiece.java
+++ src/main/java/org/amc/game/chessserver/messaging/svg/SVGBishopPiece.java
@@ -7,43 +7,48 @@ import org.w3c.dom.Element;
 
 public final class SVGBishopPiece extends SVGChessPiece {
 
-	public SVGBishopPiece(Document document, String svgNS) {
-		super(document, svgNS);
-	}
-	
-	@Override
-	public Element getChessPieceElement(String id, Location location,
-			Colour pieceColour) {
-		SVGColour svgColour = SVGColour.getColour(pieceColour);
-		float coordX = offsetXY * (location.getLetter().ordinal());
-	    float coordY = -554 - offsetXY * (location.getNumber() - 1);
-		
-	    String styleString_1 = "fill:" + svgColour.fill + ";fill-opacity:1;stroke:"  + svgColour.stroke + 
-	    		";stroke-width:0.7;stroke-linecap:butt;stroke-linejoin:round;stroke-miterlimit:4;"
-	    		+ "stroke-opacity:1;stroke-dasharray:none;stroke-dashoffset:50";
-	    
-	    String transform_1 = "translate(0,989.86218)";
-	    
-	    Element g = createG(id, "translate(" + coordX + "," + coordY + ")");
-        						
-        setTransform(appendNewRectangle(g, "14.520943", "55.176395", "35.355339", "4.0406103", styleString_1),
-                        transform_1);
-        
-        setTransform(appendNewRectangle(g, "16.793787", "50.630707", "30.430845", "4.2931485", styleString_1),
-                        transform_1);
-        setTransform(appendNewRectangle(g, "23.220133", "25.23", "17.578154", "2.9223585", styleString_1), 
-                        transform_1);
-        setTransform(appendNewPath(g, "M 39.342805,6.9601953 A 6.4397225,9.975256 0 1 1 38.535974,5.600699 l -4.317056,7.401826 z",
-                        styleString_1), "translate(-1.894036,992.13502)");  
-        
-        setTransform(appendNewPath(g, "m 34.084445,2.8670609 a 1.8940361,1.5152289 0 1 1 -0.0018,-0.00187",
-                        styleString_1), "translate(0,989.86218)");  
-        
- 
-        appendNewPath(g, "m 28.663078,1018.5221 6.565991,0 1.495423,12.3247 8.281137,9.8247 -25.792606,0 7.760741,-9.8247 z",  
-        styleString_1);
-        
-		return g;
-	}
+    public SVGBishopPiece(Document document, String svgNS) {
+        super(document, svgNS);
+    }
+
+    @Override
+    public Element getChessPieceElement(String id, Location location, Colour pieceColour) {
+        SVGColour svgColour = SVGColour.getColour(pieceColour);
+        float coordX = offsetXY * (location.getLetter().ordinal());
+        float coordY = -554 - offsetXY * (location.getNumber() - 1);
+
+        String styleString_1 = "fill:"
+                        + svgColour.fill
+                        + ";fill-opacity:1;stroke:"
+                        + svgColour.stroke
+                        + ";stroke-width:0.7;stroke-linecap:butt;stroke-linejoin:round;stroke-miterlimit:4;"
+                        + "stroke-opacity:1;stroke-dasharray:none;stroke-dashoffset:50";
+
+        String transform_1 = "translate(0,989.86218)";
+
+        Element g = createG(id, "translate(" + coordX + "," + coordY + ")");
+
+        setTransform(appendNewRectangle(g, "14.520943", "55.176395", "35.355339", "4.0406103",
+                        styleString_1), transform_1);
+
+        setTransform(appendNewRectangle(g, "16.793787", "50.630707", "30.430845", "4.2931485",
+                        styleString_1), transform_1);
+        setTransform(appendNewRectangle(g, "23.220133", "25.23", "17.578154", "2.9223585",
+                        styleString_1), transform_1);
+        setTransform(appendNewPath(
+                        g,
+                        "M 39.342805,6.9601953 A 6.4397225,9.975256 0 1 1 38.535974,5.600699 l -4.317056,7.401826 z",
+                        styleString_1), "translate(-1.894036,992.13502)");
+
+        setTransform(appendNewPath(g,
+                        "m 34.084445,2.8670609 a 1.8940361,1.5152289 0 1 1 -0.0018,-0.00187",
+                        styleString_1), "translate(0,989.86218)");
+
+        appendNewPath(g,
+                        "m 28.663078,1018.5221 6.565991,0 1.495423,12.3247 8.281137,9.8247 -25.792606,0 7.760741,-9.8247 z",
+                        styleString_1);
+
+        return g;
+    }
 
 }
--- src/main/java/org/amc/game/chessserver/messaging/svg/SVGRookPiece.java
+++ src/main/java/org/amc/game/chessserver/messaging/svg/SVGRookPiece.java
@@ -21,29 +21,35 @@ public class SVGRookPiece extends SVGChessPiece {
         g.setAttributeNS(null, "transform", "translate(" + coordX + "," + coordY + ")");
         g.setAttributeNS(null, "id", id);
 
-        
         String styleString_1 = "fill:"
                         + svgColour.fill
                         + ";fill-opacity:1;stroke:"
                         + svgColour.stroke
                         + ";stroke-width:0.44588459;stroke-linecap:butt;stroke-linejoin:round;stroke-miterlimit:3.5;stroke-opacity:0.98453603;stroke-dasharray:none;stroke-dashoffset:50";
- 
+
         String rectWidth = "7.2172222";
         String rectHeight = "4.3972631";
 
-        appendNewPath(g, "m 19.164173,457.97417 24.363471,-0.18711 3.207655,24.82314 -30.137248,-0.0935 z", styleString_1);
-        appendNewPath(g, "m 13.083803,446.84895 36.538917,0 -1.283062,11.11695 -33.652029,-0.0935 z", styleString_1);
-                
+        appendNewPath(g,
+                        "m 19.164173,457.97417 24.363471,-0.18711 3.207655,24.82314 -30.137248,-0.0935 z",
+                        styleString_1);
+
+        appendNewPath(g,
+                        "m 13.083803,446.84895 36.538917,0 -1.283062,11.11695 -33.652029,-0.0935 z",
+                        styleString_1);
+
         appendNewRectangle(g, "13.069632", "442.34985", rectWidth, rectHeight, styleString_1);
-       
+
         appendNewRectangle(g, "23.253931", "442.30307", rectWidth, rectHeight, styleString_1);
 
         appendNewRectangle(g, "42.018715", "442.30307", rectWidth, rectHeight, styleString_1);
 
         appendNewRectangle(g, "33.197662", "442.30307", rectWidth, rectHeight, styleString_1);
 
-        appendNewPath(g, "m 9.5236162,495.76948 c 12.5961078,0 32.0383488,0.37522 44.6344558,0.37522 0,-4.4898 1.120276,-10.01147 -3.609778,-13.56319 -12.596108,0 -24.943265,0 -37.539373,0 -4.3566298,2.42606 -3.4853048,8.69817 -3.4853048,13.18797 z", styleString_1);
-        
+        appendNewPath(g,
+                        "m 9.5236162,495.76948 c 12.5961078,0 32.0383488,0.37522 44.6344558,0.37522 0,-4.4898 1.120276,-10.01147 -3.609778,-13.56319 -12.596108,0 -24.943265,0 -37.539373,0 -4.3566298,2.42606 -3.4853048,8.69817 -3.4853048,13.18797 z",
+                        styleString_1);
+
         return g;
     }
 
