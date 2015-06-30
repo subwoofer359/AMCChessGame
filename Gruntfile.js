c47eb139ae3ebc26e0a56226edcf9f7876087def
Utopia <subwoofer359@gmail.com>
2015-06-30 20:11:51 +0100

    CG-79 add CSS minify and JavaScript minify to project

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         jshint: {
-            all: ['Gruntfile.js', 'src/main/webapp/js/**/*.js', 'test/**/*.js', '!src/main/webapp/js/**/bootstrap.min.js']
+            all: ['Gruntfile.js', 'src/main/webapp/jsfull/**/*.js', 'test/**/*.js', '!src/main/webapp/jsfull/**/bootstrap.min.js']
         },
         qunit: {
             all: ['src/main/webapp/WEB-INF/Tests/Qunit.html']
@@ -16,7 +16,35 @@ module.exports = function (grunt) {
                 },
                 'pre-commit' : 'jshint qunit'
             }
-        }
+        },
+	cssmin: {
+		options: {
+    			shorthandCompacting: true,
+    			roundingPrecision: -1
+  		},
+  		target: {
+    			files: [{
+      				expand: true,
+      				cwd: 'src/main/webapp/cssfull',
+      				src: ['*.css', '!*.min.css'],
+      				dest: 'src/main/webapp/css',
+      				ext: '.css'
+    			}]
+  		}
+	},
+	uglify: {
+		mangle: {
+        		except: ['jQuery', 'Backbone']
+      		},
+    		my_target: {
+      			files: [{
+        			expand: true,
+        			cwd: 'src/main/webapp/jsfull',
+        			src: ['**/*.js', '!*.min.js'],
+        			dest: 'src/main/webapp/js'
+      			}]
+    		}
+  	}
 
     });
 
@@ -24,6 +52,10 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-githooks');
     grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.registerTask('default', ['jshint', 'qunit']);
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    
+    grunt.registerTask('default', ['jshint', 'uglify', 'qunit', 'cssmin']);
     grunt.registerTask('QUnit', ['qunit']);
+    grunt.registerTask('css_min', ['cssmin']);
 };
