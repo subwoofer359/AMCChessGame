52c3a879bbdb2e999562cb9466b82b30538d86cd
Adrian McLaughlin <subwoofer359@gmail.com>
2015-10-27 13:29:52 +0000

    remove common elements into external fixture

--- src/test/java/org/amc/dao/ChessGameDAOTest.java
+++ src/test/java/org/amc/dao/ChessGameDAOTest.java
@@ -2,15 +2,11 @@ package org.amc.dao;
 
 import static org.junit.Assert.*;
 
-import org.amc.DAOException;
 import org.amc.game.chess.ChessBoard;
 import org.amc.game.chess.ChessBoardUtilities;
 import org.amc.game.chess.ChessGame;
-import org.amc.game.chess.ChessGameFixture;
-import org.amc.game.chess.ChessGamePlayer;
-import org.amc.game.chess.Colour;
+
 import org.amc.game.chess.ComparePlayers;
-import org.amc.game.chess.HumanPlayer;
 import org.amc.game.chess.Move;
 import org.amc.game.chess.Player;
 import org.amc.game.chess.SimpleInputParser;
@@ -19,16 +15,13 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.text.ParseException;
-
 public class ChessGameDAOTest {
 
     private DatabaseSignUpFixture signUpfixture = new DatabaseSignUpFixture();
     private DAO<ChessGame> chessGameDAO;
-    private ChessGameFixture cgFixture;
+
     private Player laura;
     private Player nobby;
-    private DAO<Player> playerDAO;
     private ChessGame game;
     private SimpleInputParser mlParser;
     private int id;
@@ -38,17 +31,12 @@ public class ChessGameDAOTest {
     public void setUp() throws Exception {
         this.signUpfixture.setUp();
         this.mlParser = new SimpleInputParser();
+        ServerChessGameTestDatabaseEntity serverChessGamesfixture = new ServerChessGameTestDatabaseEntity();
+        laura = serverChessGamesfixture.getBlackPlayer();
+        nobby = serverChessGamesfixture.getWhitePlayer();
+        game = serverChessGamesfixture.getChessGame();
+        chessGameDAO = new DAO<>(ChessGame.class);  
         
-        chessGameDAO = new DAO<>(ChessGame.class);
-        cgFixture = new ChessGameFixture();
-        
-        playerDAO = new DAO<Player>(HumanPlayer.class);
-        laura = playerDAO.findEntities("userName", "laura").get(0);
-        nobby = playerDAO.findEntities("userName", "nobby").get(0);
-        game = new ChessGame(cgFixture.getBoard(), new ChessGamePlayer(nobby, Colour.WHITE),
-                        new ChessGamePlayer(laura, Colour.BLACK));
-        cgFixture.getBoard().initialise();
-        chessGameDAO.addEntity(game);
         id = game.getId();
     }
 
