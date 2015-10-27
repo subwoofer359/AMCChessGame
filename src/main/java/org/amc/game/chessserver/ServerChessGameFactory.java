b96c14ef5fbd7684502fdb506d7f6ecc956efd70
Adrian McLaughlin <subwoofer359@gmail.com>
2015-10-27 15:30:07 +0000

    CG-96 modify ServerChessGameFactory#getServerChessGame to add observers on creation

--- src/main/java/org/amc/game/chessserver/ServerChessGameFactory.java
+++ src/main/java/org/amc/game/chessserver/ServerChessGameFactory.java
@@ -2,6 +2,7 @@ package org.amc.game.chessserver;
 
 import org.amc.game.chess.Player;
 import org.amc.game.chessserver.messaging.OfflineChessGameMessager;
+import org.amc.game.chessserver.observers.ObserverFactoryChain;
 import org.amc.game.chessserver.spring.OfflineChessGameMessagerFactory;
 
 import javax.annotation.Resource;
@@ -9,25 +10,38 @@ import javax.annotation.Resource;
 public class ServerChessGameFactory {
 	
     private OfflineChessGameMessagerFactory offlineChessGameMessagerFactory;
+    private ObserverFactoryChain observerFactoryChain;
+    
+    private static final String LOCAL_OBSERVERS = "JsonChessGameView|GameFinishedListener|GameStateListener";
+    private static final String NETWORK_OBSERVERS = LOCAL_OBSERVERS + "|OfflineChessGameMessager";
     
 	public enum GameType {
 		LOCAL_GAME,
 		NETWORK_GAME;
 	}
 	public ServerChessGame getServerChessGame(GameType gameType, long uid, Player player) {
-	    ServerChessGame serverGame;
+	    ServerChessGame serverChessGame;
+	    String observerStr = "";
+	    
 		switch(gameType) {
 		case LOCAL_GAME:
-			serverGame = new OneViewServerChessGame(uid, player);
+			serverChessGame = new OneViewServerChessGame(uid, player);
+			observerStr = LOCAL_OBSERVERS;
 			break;
 		case NETWORK_GAME:
 		default:
-		    serverGame = new ServerChessGame(uid, player);
-		    serverGame.attachObserver(createOfflineChessGameMessager());
+		    serverChessGame = new ServerChessGame(uid, player);
+		    serverChessGame.attachObserver(createOfflineChessGameMessager());
+		    observerStr = NETWORK_OBSERVERS;
 		    break;
 		}
-		return serverGame;
+		
+		observerFactoryChain.addObserver(observerStr, serverChessGame);
+		return serverChessGame;
 	}
+	
+	
+
 	
 	@Resource(name="offlineChessGameMessagerFactory")
     public void setOfflineChessGameMessagerFactory(OfflineChessGameMessagerFactory factory) {
@@ -37,5 +51,10 @@ public class ServerChessGameFactory {
 	private OfflineChessGameMessager createOfflineChessGameMessager() {
         return offlineChessGameMessagerFactory.createOfflineChessGameMessager();
     }
+	
+	@Resource(name = "defaultObserverFactoryChain")
+	public void setObserverFactoryChain(ObserverFactoryChain observerFactoryChain) {
+	    this.observerFactoryChain = observerFactoryChain;
+	}
 
 }
--- src/test/java/org/amc/game/chessserver/ServerChessGameFactoryTest.java
+++ src/test/java/org/amc/game/chessserver/ServerChessGameFactoryTest.java
@@ -1,15 +1,20 @@
 package org.amc.game.chessserver;
 
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
 
+import org.amc.game.GameSubject;
 import org.amc.game.chess.ChessGamePlayer;
 import org.amc.game.chess.Colour;
 import org.amc.game.chess.HumanPlayer;
 import org.amc.game.chessserver.ServerChessGameFactory.GameType;
 import org.amc.game.chessserver.messaging.OfflineChessGameMessager;
+import org.amc.game.chessserver.observers.GameFinishedListenerFactory;
+import org.amc.game.chessserver.observers.GameStateListenerFactory;
+import org.amc.game.chessserver.observers.JsonChessGameViewFactory;
+import org.amc.game.chessserver.observers.ObserverFactoryChain;
+import org.amc.game.chessserver.observers.ObserverFactoryChainImpl;
 import org.amc.game.chessserver.spring.OfflineChessGameMessagerFactory;
-import org.amc.util.DefaultSubject;
 import org.amc.util.Observer;
 import org.junit.After;
 import org.junit.Before;
@@ -39,8 +44,28 @@ public class ServerChessGameFactoryTest {
             }
             
         };
-        
         scgfactory.setOfflineChessGameMessagerFactory(factory);
+        scgfactory.setObserverFactoryChain(getUpObserverFactoryChain());
+        
+    }
+    
+    public ObserverFactoryChain getUpObserverFactoryChain() {
+        JsonChessGameViewFactory jsonviewFactory = new JsonChessGameViewFactory();
+        GameFinishedListenerFactory gameFinishedListenerFactory = new GameFinishedListenerFactory();
+        GameStateListenerFactory gameStateListenerFactory = new GameStateListenerFactory();
+        
+        ObserverFactoryChain jsonViewChain = new ObserverFactoryChainImpl();
+        jsonViewChain.setObserverFactory(jsonviewFactory);
+        
+        ObserverFactoryChain gFLChain = new ObserverFactoryChainImpl();
+        gFLChain.setObserverFactory(gameFinishedListenerFactory);
+        
+        ObserverFactoryChain gSLChain = new ObserverFactoryChainImpl();
+        gSLChain.setObserverFactory(gameStateListenerFactory);
+        
+        jsonViewChain.setSuccessor(gFLChain);
+        gFLChain.setSuccessor(gSLChain);
+        return jsonViewChain;
     }
 
     @After
@@ -53,6 +78,7 @@ public class ServerChessGameFactoryTest {
         assertTrue(scgGame instanceof ServerChessGame);
         assertFalse(scgGame instanceof OneViewServerChessGame);
         checkOfflineChessGameMessagerHasBeenAdded(scgGame, true);
+        assertEquals(4, scgGame.getNoOfObservers());
     }
     
     @Test
@@ -61,10 +87,11 @@ public class ServerChessGameFactoryTest {
         assertTrue(scgGame instanceof ServerChessGame);
         assertTrue(scgGame instanceof OneViewServerChessGame);
         checkOfflineChessGameMessagerHasBeenAdded(scgGame, false);
+        assertEquals(3, scgGame.getNoOfObservers());
     }
     
     private void checkOfflineChessGameMessagerHasBeenAdded(ServerChessGame scgGame, boolean pass) throws Exception {
-        Field observerField = DefaultSubject.class.getDeclaredField("observers");
+        Field observerField = GameSubject.class.getDeclaredField("observers");
         observerField.setAccessible(true);
         
         List<Observer> list = (List<Observer>)observerField.get(scgGame);
