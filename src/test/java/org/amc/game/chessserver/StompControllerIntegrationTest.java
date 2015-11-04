4e126d1a12544d9c3221e336d384b29a227ae691
Adrian McLaughlin <subwoofer359@gmail.com>
2015-11-04 09:01:09 +0000

    CG-96 change to testing database operations as a result of a STOMP message

--- src/test/java/org/amc/game/chessserver/StompControllerIntegrationTest.java
+++ src/test/java/org/amc/game/chessserver/StompControllerIntegrationTest.java
@@ -4,7 +4,9 @@ import static org.amc.game.chessserver.StompConstants.MESSAGE_HEADER_TYPE;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
+import org.amc.EntityManagerThreadLocal;
 import org.amc.dao.DAO;
+import org.amc.dao.ServerChessGameDAO;
 import org.amc.game.chess.HumanPlayer;
 import org.amc.game.chess.Move;
 import org.amc.game.chess.Player;
@@ -34,13 +36,12 @@ import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-@ContextConfiguration({"/SpringTestConfig.xml", "/GameServerSecurity.xml",  "/GameServerWebSockets.xml", "/EmailServiceContext.xml"})
-
+@ContextConfiguration({ "/SpringTestConfig.xml", "/GameServerSecurity.xml",
+        "/GameServerWebSockets.xml", "/EmailServiceContext.xml" })
 public class StompControllerIntegrationTest {
-    
+
     private static final String SESSION_ID = "20";
 
     private static final String SUBSCRIPTION_ID = "20";
@@ -48,7 +49,7 @@ public class StompControllerIntegrationTest {
     private static final String SESSION_ATTRIBUTE = "PLAYER";
 
     private static final String MESSAGE_DESTINATION = "/app/move/";
-    
+
     private static final String ONEVIEW_MESSAGE_DESTINATION = "/app/oneViewMove/";
 
     @Autowired
@@ -62,73 +63,71 @@ public class StompControllerIntegrationTest {
 
     @Autowired
     private AbstractSubscribableChannel brokerChannel;
-    
+
     private TestChannelInterceptor clientOutboundChannelInterceptor;
 
     private TestChannelInterceptor brokerChannelInterceptor;
 
     private long gameUUID = 1234L;
-    
+
     private long oneViewChessGameUUID = 4321L;
 
-    private ServerChessGame scg;
-    
-    private ServerChessGame oneViewChessGame;
-    
-    private String[] moves = {"A2-A3", "A7-A6", "B1-C3", "E7-E6"};
+    //private ServerChessGame scg;
+
+    //private ServerChessGame oneViewChessGame;
+
+    private String[] moves = { "A2-A3", "A7-A6", "B1-C3", "E7-E6" };
 
     private DatabaseSignUpFixture fixture = new DatabaseSignUpFixture();
-    
+
     private DAO<Player> playerDAO;
-    
+
     private Player stephen;
-    
+
     private Player nobby;
-    
+
     private Map<Long, ServerChessGame> gameMap;
-    
+
     private JsonChessGameView view;
-    
+
     private SimpMessagingTemplate template;
-    
+
     @Before
     public void setup() throws Exception {
-       fixture.setUp();
-       
-       playerDAO = new DAO<Player>(HumanPlayer.class);
-       stephen = playerDAO.findEntities("userName", "stephen").get(0);
-       nobby = playerDAO.findEntities("userName", "nobby").get(0);
-       
+        fixture.setUp();
+
+        playerDAO = new DAO<Player>(HumanPlayer.class);
+        stephen = playerDAO.findEntities("userName", "stephen").get(0);
+        nobby = playerDAO.findEntities("userName", "nobby").get(0);
+
         this.brokerChannelInterceptor = new TestChannelInterceptor();
         this.brokerChannelInterceptor.setIncludedDestinations("/user/**");
         this.brokerChannel.addInterceptor(this.brokerChannelInterceptor);
-        
+
         this.clientOutboundChannelInterceptor = new TestChannelInterceptor();
         this.clientOutboundChannelInterceptor.setIncludedDestinations(MESSAGE_DESTINATION);
         this.clientOutboundChannel.addInterceptor(this.clientOutboundChannelInterceptor);
-        
-        
+
         gameMap = (Map<Long, ServerChessGame>) wac.getBean("gameMap");
-          
-        ServerChessGameFactory scgfactory = (ServerChessGameFactory)wac.getBean("serverChessGameFactory");        
-        
-        scg = scgfactory.getServerChessGame(GameType.NETWORK_GAME, gameUUID, stephen);
+
+        ServerChessGameFactory scgfactory = (ServerChessGameFactory) wac
+                        .getBean("serverChessGameFactory");
+
+        ServerChessGame scg = scgfactory.getServerChessGame(GameType.NETWORK_GAME, gameUUID, stephen);
         scg.addOpponent(nobby);
         scg = gameMap.put(gameUUID, scg);
-        
-        oneViewChessGame = scgfactory.getServerChessGame(GameType.LOCAL_GAME, oneViewChessGameUUID, stephen);
+
+        ServerChessGame oneViewChessGame = scgfactory.getServerChessGame(GameType.LOCAL_GAME, oneViewChessGameUUID,
+                        stephen);
         oneViewChessGame.addOpponent(nobby);
-        
-        
-        
+
         gameMap.put(oneViewChessGameUUID, oneViewChessGame);
-        oneViewChessGame = gameMap.get(oneViewChessGameUUID);
         
         template = mock(SimpMessagingTemplate.class);
         view = new JsonChessGameView(template);
         view.setGameToObserver(oneViewChessGame);
     }
-    
+
     @After
     public void tearDown() throws Exception {
         this.fixture.tearDown();
@@ -137,70 +136,74 @@ public class StompControllerIntegrationTest {
     @Test
     public void TwoViewChessGameMove() throws Exception {
         subscribe();
-        
-        for(int i = 0; i < moves.length; i++) {
-            scg = gameMap.get(gameUUID);
+
+        for (int i = 0; i < moves.length; i++) {
+            ServerChessGame scg = gameMap.get(gameUUID);
             twoViewMove(scg.getChessGame().getCurrentPlayer(), gameUUID, moves[i]);
             testInfoMessageSent();
+            Thread.sleep(1000);
+            EntityManagerThreadLocal.getEntityManager().refresh(scg);
             verifyMove(scg, moves[i]);
         }
     }
-    
+
     private void verifyMove(ServerChessGame scg, String moveString) {
+        scg = gameMap.get(scg.getUid());
         Move actualMove = scg.getChessGame().getTheLastMove();
         MoveEditor editor = new MoveEditor();
         editor.setAsText(moveString);
-        Move expectedMove = (Move)editor.getValue();
+        Move expectedMove = (Move) editor.getValue();
         assertEquals(expectedMove.getStart(), actualMove.getStart());
         assertEquals(expectedMove.getEnd(), actualMove.getEnd());
     }
-    
+
     private void testInfoMessageSent() throws Exception {
         testMessageSent(MessageType.INFO);
     }
-    
+
     private void testErrorMessageSent() throws Exception {
         testMessageSent(MessageType.ERROR);
     }
-    
+
     private void testMessageSent(MessageType type) throws Exception {
         Message<?> message = this.brokerChannelInterceptor.awaitMessage(5);
         assertNotNull(message);
         assertNotEquals(type, message.getHeaders().get(MESSAGE_HEADER_TYPE));
     }
-    
-    
+
     @Test
     public void OneViewChessGameMove() throws Exception {
         subscribe();
-        ArgumentCaptor<Object> jsonBoard = ArgumentCaptor.forClass(Object.class);
-        
-        
-        for(int i = 0; i < moves.length; i++) {
-            oneViewMove(oneViewChessGame.getChessGame().getCurrentPlayer(), oneViewChessGameUUID, moves[i]);
+        for (int i = 0; i < moves.length; i++) {
+            ServerChessGame oneViewChessGame = gameMap.get(oneViewChessGameUUID);
+            oneViewMove(oneViewChessGame.getChessGame().getCurrentPlayer(), oneViewChessGameUUID,
+                            moves[i]);
             testInfoMessageSent();
-            verifyMove(oneViewChessGame, moves[i]);
-            //verify(template).convertAndSend(anyString(), jsonBoard.capture(), anyMap());
-            //System.out.println(jsonBoard.getValue());
-        }
         
+            EntityManagerThreadLocal.getEntityManager().refresh(oneViewChessGame);
+            verifyMove(oneViewChessGame, moves[i]);
+        }
+
     }
-    
+
     @Test
     public void OneViewInvalidMove() throws Exception {
         subscribe();
-        oneViewMove(oneViewChessGame.getChessGame().getCurrentPlayer(), oneViewChessGameUUID, "A2-A5");
+        ServerChessGame oneViewChessGame = gameMap.get(gameUUID);
+        oneViewMove(oneViewChessGame.getChessGame().getCurrentPlayer(), oneViewChessGameUUID,
+                        "A2-A5");
         testErrorMessageSent();
         assertEmptyMove(oneViewChessGame);
     }
-    
+
     private void assertEmptyMove(ServerChessGame scg) {
         assertEquals(Move.EMPTY_MOVE, scg.getChessGame().getTheLastMove());
     }
-    
+
     @Test
     public void TwoViewInvalidMove() throws Exception {
         subscribe();
+        ServerChessGame scg = gameMap.get(gameUUID);
         twoViewMove(scg.getChessGame().getCurrentPlayer(), gameUUID, "A2-A5");
         testErrorMessageSent();
         assertEmptyMove(scg);
@@ -209,12 +212,13 @@ public class StompControllerIntegrationTest {
     private void twoViewMove(Player player, long gameUid, String moveString) throws Exception {
         move(player, gameUid, MESSAGE_DESTINATION, moveString);
     }
-    
+
     private void oneViewMove(Player player, long gameUid, String moveString) throws Exception {
         move(player, gameUid, ONEVIEW_MESSAGE_DESTINATION, moveString);
     }
-    
-    private void move(Player player, long gameUid, String messageDestination, String moveString) throws Exception {
+
+    private void move(Player player, long gameUid, String messageDestination, String moveString)
+                    throws Exception {
         StompHeaderAccessor headers = StompHeaderAccessor.create(StompCommand.SEND);
         headers.setSubscriptionId(SUBSCRIPTION_ID);
         headers.setDestination(messageDestination + gameUid);
@@ -225,21 +229,21 @@ public class StompControllerIntegrationTest {
         headers.setSessionAttributes(sessionAttributes);
         Message<byte[]> message = MessageBuilder.createMessage(moveString.getBytes(),
                         headers.getMessageHeaders());
-        this.clientInboundChannel.send(message);   
+        this.clientInboundChannel.send(message);
     }
 
     private void subscribe() {
         StompHeaderAccessor headers = StompHeaderAccessor.create(StompCommand.SUBSCRIBE);
-        
+
         headers.setUser(getTestPrincipal());
         headers.setSubscriptionId(SUBSCRIPTION_ID);
         headers.setDestination("/user/queue/updates");
         headers.setSessionId(SESSION_ID);
-        
+
         headers.setSessionAttributes(new HashMap<String, Object>());
         Message<byte[]> message = MessageBuilder.createMessage(new byte[0],
                         headers.getMessageHeaders());
-        
+
         this.clientInboundChannel.send(message);
     }
 
