edc91fec4fd1b234f43f596bfb98cd7baf916444
Adrian McLaughlin <subwoofer359@gmail.com>
2015-11-04 09:09:53 +0000

    CG-96 move ChessGameDAO class into ChessGameDAOTest as only needed for testing

--- src/test/java/org/amc/dao/ChessGameDAOTest.java
+++ src/test/java/org/amc/dao/ChessGameDAOTest.java
@@ -2,6 +2,7 @@ package org.amc.dao;
 
 import static org.junit.Assert.*;
 
+import org.amc.DAOException;
 import org.amc.EntityManagerThreadLocal;
 import org.amc.game.chess.ChessBoard;
 import org.amc.game.chess.ChessBoardUtilities;
@@ -12,7 +13,7 @@ import org.amc.game.chess.Player;
 import org.amc.game.chess.SimpleInputParser;
 import org.amc.game.chess.view.ChessBoardView;
 import org.amc.game.chessserver.DatabaseSignUpFixture;
-
+import org.apache.openjpa.persistence.OpenJPAEntityManager;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -81,5 +82,41 @@ public class ChessGameDAOTest {
         
         game = chessGameDAO.getEntity(id);
         ChessBoardUtilities.compareBoards(newBoard, game.getChessBoard());
+    }
+    
+    /**
+     * Test DAO not for Production use
+     * @author Adrian Mclaughlin
+     *
+     */
+    public static class ChessGameDAO extends DAO<ChessGame> {
+
+        public ChessGameDAO() {
+            super(ChessGame.class);
+        }
+        
+        
+        /**
+         * @see DAO#getEntity(int)
+         * manually marks the field board of ChessBoard dirty so it's updated
+         * when the EntityManager is closed. An implementation solution
+         * @throws DAOException when Database query fails or not using an OpenJPAEntityManager
+         * TODO Replace with an agnostic solution
+         */
+        @Override
+        public ChessGame getEntity(int id) throws DAOException {
+            ChessGame game = super.getEntity(id);
+            if(getEntityManager() instanceof OpenJPAEntityManager) {
+                if(game != null) {
+                    ((OpenJPAEntityManager)getEntityManager()).dirty(game, "board");
+                }
+            } else {
+                throw new DAOException("Not an OpenJPA entity manager: changes to chessboard won't be saved");
+            }
+            
+            return game;
+        }
+
+        
     }
 }
