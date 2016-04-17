e72f667e011d7d57c2eacd9ac0f156908bf7f8f5
Adrian McLaughlin <subwoofer359@gmail.com>
2016-04-17 06:10:55 +0100

    Use EntityManagerCache for retrieving EntityManagers

--- src/main/java/org/amc/dao/ServerChessGameDAO.java
+++ src/main/java/org/amc/dao/ServerChessGameDAO.java
@@ -33,84 +33,89 @@ import javax.persistence.TypedQuery;
 public class ServerChessGameDAO extends DAO<AbstractServerChessGame> {
 
     private static final Logger logger = Logger.getLogger(ServerChessGameDAO.class);
-    
+
     static final String GET_SERVERCHESSGAME_QUERY = "serverChessGameByUid";
     private static final String NATIVE_OBSERVERS_QUERY = "Select uid,observers from serverChessGames where uid = ?1";
-    
+
     private ObserverFactoryChain chain;
     private EntityManagerCache entityManagerCache;
-    
+
     public ServerChessGameDAO() {
         super(ServerChessGame.class);
     }
-    
+
     @Deprecated
     public Set<Long> getGameUids() {
         EntityManager entityManager = getEntityManager();
-        Query query = entityManager.createQuery("Select x.uid from " + getEntityClass().getSimpleName() + " x ORDER BY x.uid");
+        Query query = entityManager.createQuery("Select x.uid from "
+                        + getEntityClass().getSimpleName() + " x ORDER BY x.uid");
         try {
-        	@SuppressWarnings("unchecked")
+            @SuppressWarnings("unchecked")
             Set<Long> gameUidsSet = new HashSet<Long>(query.getResultList());
-        	return gameUidsSet;
-        } catch(PersistenceException pe) {
-        	logger.error(pe);
-        	return Collections.<Long>emptySet();
+            return gameUidsSet;
+        } catch (PersistenceException pe) {
+            logger.error(pe);
+            return Collections.<Long> emptySet();
         }
-        
+
     }
-    
+
     void addObservers(AbstractServerChessGame serverChessGame) throws DAOException {
         EntityManager entityManager = getEntityManager();
-        if(serverChessGame.getNoOfObservers() == 0) {
-            Query query = entityManager.createNativeQuery(NATIVE_OBSERVERS_QUERY, SCGObservers.class);
+        if (serverChessGame.getNoOfObservers() == 0) {
+            Query query = entityManager.createNativeQuery(NATIVE_OBSERVERS_QUERY,
+                            SCGObservers.class);
             query.setParameter(1, serverChessGame.getUid());
             try {
-                SCGObservers scgObervers = (SCGObservers)query.getSingleResult();
+                SCGObservers scgObervers = (SCGObservers) query.getSingleResult();
                 chain.addObserver(scgObervers.getObservers(), serverChessGame);
-            } catch(NoResultException nre) {
+            } catch (NoResultException nre) {
                 logger.error(nre);
             }
         }
     }
-    
+
     /**
      * 
-     * @param uid unique identifier of the ServerChessGame to be retrieved
+     * @param uid
+     *            unique identifier of the ServerChessGame to be retrieved
      * @return ServerChessGame with Observers attached
-     * @throws DAOException if the ServerChessGame can't be retrieved
+     * @throws DAOException
+     *             if the ServerChessGame can't be retrieved
      */
     public AbstractServerChessGame getServerChessGame(long uid) throws DAOException {
         EntityManager entityManager = getEntityManager(uid);
         Query query = entityManager.createNamedQuery(GET_SERVERCHESSGAME_QUERY);
         query.setParameter(1, uid);
-        
+
         AbstractServerChessGame scg = null;
         try {
-            scg = (AbstractServerChessGame)query.getSingleResult();
-            if(scg.getNoOfObservers() == 0) {
+            scg = (AbstractServerChessGame) query.getSingleResult();
+            if (scg.getNoOfObservers() == 0) {
                 addObservers(scg);
             }
             scg.setChessGameFactory(new StandardChessGameFactory());
             return scg;
-        } catch(NoResultException nre) {
+        } catch (NoResultException nre) {
             logger.error(nre);
         }
         return scg;
     }
-    
-    public AbstractServerChessGame saveServerChessGame(AbstractServerChessGame serverChessGame) throws DAOException {
+
+    public AbstractServerChessGame saveServerChessGame(AbstractServerChessGame serverChessGame)
+                    throws DAOException {
         EntityManager em = getEntityManager(serverChessGame.getUid());
         try {
             em.getTransaction().begin();
-            if(em.contains(serverChessGame)) {
-                
+            if (em.contains(serverChessGame)) {
+
             } else {
-                
+
                 serverChessGame = em.merge(serverChessGame);
             }
-            
-            if(em instanceof OpenJPAEntityManager) {
-                ((OpenJPAEntityManager)em).dirty(serverChessGame.getChessGame(), "board");
+
+            if (em instanceof OpenJPAEntityManager) {
+                ((OpenJPAEntityManager) em).dirty(serverChessGame.getChessGame(), "board");
             }
             em.flush();
             em.getTransaction().commit();
@@ -121,40 +126,41 @@ public class ServerChessGameDAO extends DAO<AbstractServerChessGame> {
             throw new DAOException(pe);
         }
     }
-    
+
     /**
-     * Returns Games for display purposes only. 
+     * Returns Games for display purposes only.
      * Certain fields aren't initialised
      * If a fully initialised is required use {@link #getServerChessGame(long)}
      * 
      * @param player
      * @return Map Of partially initialised ServerChessGames
-     * @throws DAOException if database operation raises an exception 
+     * @throws DAOException
+     *             if database operation raises an exception
      */
     public Map<Long, ServerChessGame> getGamesForPlayer(Player player) throws DAOException {
         EntityManager em = getEntityManager();
-        TypedQuery<ServerChessGame> query = em.createNamedQuery("getChessGamesByPlayer", 
+        TypedQuery<ServerChessGame> query = em.createNamedQuery("getChessGamesByPlayer",
                         ServerChessGame.class);
         query.setParameter(1, player.getUserName());
         try {
             Map<Long, ServerChessGame> games = new HashMap<Long, ServerChessGame>();
             List<ServerChessGame> results = query.getResultList();
-            for(ServerChessGame game : results) {
+            for (ServerChessGame game : results) {
                 games.put(game.getUid(), game);
             }
-            return  games;
+            return games;
         } catch (PersistenceException pe) {
             logger.error(pe);
             em.close();
             throw new DAOException(pe);
         }
     }
-    
+
     public EntityManager getEntityManager(Long gameUid) {
         return entityManagerCache.getEntityManager(gameUid);
     }
-    
-    @Resource(name="defaultObserverFactoryChain")
+
+    @Resource(name = "defaultObserverFactoryChain")
     public void setObserverFactoryChain(ObserverFactoryChain chain) {
         this.chain = chain;
     }
@@ -163,11 +169,11 @@ public class ServerChessGameDAO extends DAO<AbstractServerChessGame> {
     public void setEntityManagerCache(EntityManagerCache entityManagerCache) {
         this.entityManagerCache = entityManagerCache;
     }
-    
-    
+
     /**
      * Helper Class to retrieve the list of observers in String
      * format in the database
+     * 
      * @author Adrian Mclaughlin
      *
      */
@@ -179,16 +185,13 @@ public class ServerChessGameDAO extends DAO<AbstractServerChessGame> {
             return observers;
         }
 
-
         public Long getUid() {
             return uid;
         }
 
-
         public void setObservers(String observers) {
             this.observers = observers;
         }
-
 
         public void setUid(Long uid) {
             this.uid = uid;
