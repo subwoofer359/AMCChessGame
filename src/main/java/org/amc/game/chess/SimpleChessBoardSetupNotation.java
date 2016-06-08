057955285f4dc2bff8671b243ef7dd9028202eb2
Utopia <subwoofer359@gmail.com>
2016-06-08 20:09:37 +0100

    CG-102 fix

--- src/main/java/org/amc/game/chess/SimpleChessBoardSetupNotation.java
+++ src/main/java/org/amc/game/chess/SimpleChessBoardSetupNotation.java
@@ -21,12 +21,14 @@ public class SimpleChessBoardSetupNotation implements ChessBoardSetupNotation {
     /**
      * Regex used for matching the components of the string input
      */
-    private static final Pattern inputPattern = Pattern.compile("([KQBNRPkqbnrp][a-h][1-8]):?");
+    private static final Pattern inputPattern = Pattern.compile("([KQBNRPkqbnrp][a-h][1-8]#?):?");
 
     /**
      * Regex used for matching the whole string input
      */
     private static final Pattern wholePattern = Pattern.compile("(" + inputPattern + ")+");
+    
+    public static final String MOVE_TOKEN = "#";
 
     /**
      * Checks the input string to see if can be parsed by this class
@@ -62,6 +64,10 @@ public class SimpleChessBoardSetupNotation implements ChessBoardSetupNotation {
     private ChessPieceLocation parseGroup(String group) throws ParseException {
         ChessPiece piece = getChessPiece(group.substring(0, 1));
         Location location = new Location(group.substring(1, 3));
+        if(group.endsWith(MOVE_TOKEN)) {
+        	piece.moved();
+        }
+        
         return new ChessPieceLocation(piece, location);
     }
 
