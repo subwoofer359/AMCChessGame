e03ef7fc4d60017c433df6e76e7ad12d78981848
Adrian McLaughlin <subwoofer359@gmail.com>
2016-07-05 09:56:29 +0100

    clean up tabs and spaces in Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,26 +3,26 @@ module.exports = function (grunt) {
     "use strict";
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-	env: {
-		coverage: {
-			APP_DIR_FOR_CODE_COVERAGE: 'test/coverage/instrument/app/'
-		}
-	},
+        env: {
+            coverage: {
+                APP_DIR_FOR_CODE_COVERAGE: 'test/coverage/instrument/app/'
+            }
+        },
         jshint: {
             all: ['Gruntfile.js', 'src/main/webapp/jsfull/**/*.js', 'test/**/*.js', '!src/main/webapp/jsfull/**/bootstrap.min.js']
         },
         qunit: {
-                all: ['src/main/webapp/WEB-INF/Tests/Qunit.html'],
-                options: {
+            all: ['src/main/webapp/WEB-INF/Tests/Qunit.html'],
+            options: {
                 '--web-security': 'no',
                 console: true,
                 coverage: {
-                   disposeCollector: true,
-                   src: ['src/main/webapp/jsfull/*.js'],
-                   instrumentedFiles: 'test/coverage/instrument',
-                   htmlReport: 'report/coverage',
-                   coberturaReport: 'report/',
-                   linesThresholdPct: 84
+                    disposeCollector: true,
+                    src: ['src/main/webapp/jsfull/*.js'],
+                    instrumentedFiles: 'test/coverage/instrument',
+                    htmlReport: 'report/coverage',
+                    coberturaReport: 'report/',
+                    linesThresholdPct: 84
                 }
             }
         },
@@ -34,35 +34,34 @@ module.exports = function (grunt) {
                 'pre-commit' : 'jshint qunit'
             }
         },
-	cssmin: {
-		options: {
-    			shorthandCompacting: true,
-    			roundingPrecision: -1
-  		},
-  		target: {
-    			files: [{
-      				expand: true,
-      				cwd: 'src/main/webapp/cssfull',
-      				src: ['*.css', '!*.min.css'],
-      				dest: 'src/main/webapp/css',
-      				ext: '.css'
-    			}]
-  		}
-	},
-	uglify: {
-		mangle: {
-        		except: ['jQuery', 'Backbone']
-      		},
-    		my_target: {
-      			files: [{
-        			expand: true,
-        			cwd: 'src/main/webapp/jsfull',
-        			src: ['**/*.js', '!*.min.js'],
-        			dest: 'src/main/webapp/js'
-      			}]
-    		}
-  	}
-
+        cssmin: {
+            options: {
+                shorthandCompacting: true,
+                roundingPrecision: -1
+            },
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/main/webapp/cssfull',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'src/main/webapp/css',
+                    ext: '.css'
+                }]
+            }
+        },
+        uglify: {
+            mangle: {
+                except: ['jQuery', 'Backbone']
+            },
+            my_target: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/main/webapp/jsfull',
+                    src: ['**/*.js', '!*.min.js'],
+                    dest: 'src/main/webapp/js'
+                }]
+            }
+        }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
@@ -71,7 +70,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-qunit-istanbul');
-    
+
     grunt.registerTask('default', ['jshint', 'uglify', 'qunit', 'cssmin']);
     grunt.registerTask('QUnit', ['qunit']);
     grunt.registerTask('css_min', ['cssmin']);
