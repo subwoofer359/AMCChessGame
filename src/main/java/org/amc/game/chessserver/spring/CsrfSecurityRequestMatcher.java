7d2a1c7080dd65b556d15748e095eaf6e61db04b
Adrian McLaughlin <subwoofer359@gmail.com>
2016-07-05 11:37:20 +0100

    fix unnecessary if else statement

--- src/main/java/org/amc/game/chessserver/spring/CsrfSecurityRequestMatcher.java
+++ src/main/java/org/amc/game/chessserver/spring/CsrfSecurityRequestMatcher.java
@@ -1,6 +1,5 @@
 package org.amc.game.chessserver.spring;
 
-import org.apache.log4j.Logger;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import org.springframework.security.web.util.matcher.RequestMatcher;
@@ -10,8 +9,6 @@ import java.util.regex.Pattern;
 import javax.servlet.http.HttpServletRequest;
 
 public class CsrfSecurityRequestMatcher implements RequestMatcher {
-    
-    private static final Logger logger = Logger.getLogger(CsrfSecurityRequestMatcher.class);
 
     private Pattern allowedMethods = Pattern.compile("^(GET|HEAD|TRACE|OPTIONS)$");
     private AntPathRequestMatcher webSocketPathRequestMatcher = new AntPathRequestMatcher("/app/chessgame/chessgame/**");
@@ -23,11 +20,8 @@ public class CsrfSecurityRequestMatcher implements RequestMatcher {
             return false;
         } else if(webSocketPathRequestMatcher.matches(request)) {
             return false;
-        } else if (topAntPathRequestMatcher.matches(request)){
-            return true;
-        }
-        else {
-            return false;
+        } else {
+            return topAntPathRequestMatcher.matches(request);
         }
     }
 
