c10ff85886618644576e3720b613b1790badc833
Adrian McLaughlin <subwoofer359@gmail.com>
2016-07-06 09:19:53 +0100

    use ChessBoardUtilities in KingPieceTest

--- src/test/java/org/amc/game/chess/KingPieceTest.java
+++ src/test/java/org/amc/game/chess/KingPieceTest.java
@@ -3,60 +3,52 @@ package org.amc.game.chess;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 
 import java.util.HashSet;
 import java.util.Set;
 
 public class KingPieceTest {
     private ChessBoard board;
+    private ChessBoardUtilities cbUtils;
 
     @Before
     public void setUp() throws Exception {
         board = new ChessBoard();
+        cbUtils = new ChessBoardUtilities(board);
     }
 
     @Test
     public void testIsValidMoveOnEmptyBoard() {
         KingPiece king = KingPiece.getKingPiece(Colour.BLACK);
-        addChessPieceToBoard(king, "F7");
-        assertTrue(king.isValidMove(board, createMove("F7", "F8")));
-        assertTrue(king.isValidMove(board, createMove("F7", "F6")));
-        assertTrue(king.isValidMove(board, createMove("F7", "G7")));
-        assertTrue(king.isValidMove(board, createMove("F7", "E7")));
+        cbUtils.addChessPieceToBoard(king, "F7");
+        assertTrue(king.isValidMove(board, cbUtils.createMove("F7", "F8")));
+        assertTrue(king.isValidMove(board, cbUtils.createMove("F7", "F6")));
+        assertTrue(king.isValidMove(board, cbUtils.createMove("F7", "G7")));
+        assertTrue(king.isValidMove(board, cbUtils.createMove("F7", "E7")));
 
-        assertTrue(king.isValidMove(board, createMove("F7", "E8")));
-        assertTrue(king.isValidMove(board, createMove("F7", "G8")));
-        assertTrue(king.isValidMove(board, createMove("F7", "G6")));
-        assertTrue(king.isValidMove(board, createMove("F7", "E8")));
-    }
-    
-    private Move createMove(String start, String end) {
-        return new Move(start + Move.MOVE_SEPARATOR + end);
-    }
-    
-    private void addChessPieceToBoard(ChessPiece piece, String location) {
-        board.putPieceOnBoardAt(piece, new Location(location));
+        assertTrue(king.isValidMove(board, cbUtils.createMove("F7", "E8")));
+        assertTrue(king.isValidMove(board, cbUtils.createMove("F7", "G8")));
+        assertTrue(king.isValidMove(board, cbUtils.createMove("F7", "G6")));
+        assertTrue(king.isValidMove(board, cbUtils.createMove("F7", "E8")));
     }
 
     @Test
     public void testIsNotValidMoveOnEmptyBoard() {
         KingPiece king = KingPiece.getKingPiece(Colour.BLACK);
-        addChessPieceToBoard(king, "F7");
-        assertFalse(king.isValidMove(board, createMove("F7", "A8")));
-        assertFalse(king.isValidMove(board, createMove("F7", "B6")));
-        assertFalse(king.isValidMove(board, createMove("F7", "G3")));
+        cbUtils.addChessPieceToBoard(king, "F7");
+        assertFalse(king.isValidMove(board, cbUtils.createMove("F7", "A8")));
+        assertFalse(king.isValidMove(board, cbUtils.createMove("F7", "B6")));
+        assertFalse(king.isValidMove(board, cbUtils.createMove("F7", "G3")));
         assertFalse(king.isValidMove(board,
-                        createMove("F7", StartingSquare.WHITE_KING.getLocation().asString())));
+        		cbUtils.createMove("F7", StartingSquare.WHITE_KING.getLocation().asString())));
 
-        assertFalse(king.isValidMove(board, createMove("F7", "D2")));
-        assertFalse(king.isValidMove(board, createMove("F7", "C3")));
-        assertFalse(king.isValidMove(board, createMove("F7", "G3")));
-        assertFalse(king.isValidMove(board, createMove("F7", "D8")));
+        assertFalse(king.isValidMove(board, cbUtils.createMove("F7", "D2")));
+        assertFalse(king.isValidMove(board, cbUtils.createMove("F7", "C3")));
+        assertFalse(king.isValidMove(board, cbUtils.createMove("F7", "G3")));
+        assertFalse(king.isValidMove(board, cbUtils.createMove("F7", "D8")));
 
-        assertFalse(king.isValidMove(board, createMove("F7", "F7")));
+        assertFalse(king.isValidMove(board, cbUtils.createMove("F7", "F7")));
 
     }
 
@@ -65,15 +57,15 @@ public class KingPieceTest {
         KingPiece king = KingPiece.getKingPiece(Colour.BLACK);
         KingPiece kingWhite = KingPiece.getKingPiece(Colour.WHITE);
         KingPiece anotherBlackKing = KingPiece.getKingPiece(Colour.BLACK);
-        addChessPieceToBoard(king, "F7");
-        addChessPieceToBoard(kingWhite, "E7");
-        addChessPieceToBoard(anotherBlackKing, "G7");
+        cbUtils.addChessPieceToBoard(king, "F7");
+        cbUtils.addChessPieceToBoard(kingWhite, "E7");
+        cbUtils.addChessPieceToBoard(anotherBlackKing, "G7");
 
         // Move to square occupied by the white king should be valid
-        boolean isValid = king.isValidMove(board, createMove("F7", "E7"));
+        boolean isValid = king.isValidMove(board, cbUtils.createMove("F7", "E7"));
         // Move to square occupied by the another king should not be valid
         boolean notValid = king
-                        .isValidMove(board, createMove("F7", "G7"));
+                        .isValidMove(board, cbUtils.createMove("F7", "G7"));
 
         assertTrue(isValid);
         assertFalse(notValid);
@@ -82,7 +74,7 @@ public class KingPieceTest {
     @Test
     public void testGetPossibleMoveLocations() {
         KingPiece whiteKing = KingPiece.getKingPiece(Colour.WHITE);
-        addChessPieceToBoard(whiteKing, StartingSquare.WHITE_KING.getLocation().asString());
+        cbUtils.addChessPieceToBoard(whiteKing, StartingSquare.WHITE_KING.getLocation().asString());
         Set<Location> expectedMoveLocations = new HashSet<>();
         expectedMoveLocations.add(new Location("D1"));
         expectedMoveLocations.add(new Location("D2"));
