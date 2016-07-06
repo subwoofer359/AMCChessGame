e593800f2839078aea04abe5ca20aeaa57f0e0c9
Adrian McLaughlin <subwoofer359@gmail.com>
2016-07-06 09:15:23 +0100

    move methods into ChessBoardUtilities

--- src/test/java/org/amc/game/chess/ChessBoardUtilities.java
+++ src/test/java/org/amc/game/chess/ChessBoardUtilities.java
@@ -7,8 +7,26 @@ import org.amc.game.chess.ChessBoard.Coordinate;
 import org.amc.game.chess.view.ChessBoardView;
 
 public class ChessBoardUtilities {
-
-    private ChessBoardUtilities() {
+	private ChessBoard board;
+     
+    public ChessBoardUtilities(ChessBoard board) {
+        this.board = board;
+    }
+    
+    public Move createMove(String start, String end) {
+        return new Move(start + Move.MOVE_SEPARATOR + end);
+    }
+    
+    public void addChessPieceToBoard(ChessPiece piece, String location) {
+        board.putPieceOnBoardAt(piece, new Location(location));
+    }
+    
+    public void addPawnPieceToBoard(Colour colour, String location) {
+        addChessPieceToBoard(PawnPiece.getPawnPiece(colour), location);
+    }
+    
+    public ChessPiece getPieceOnBoard(String location) {
+        return board.getPieceFromBoardAt(new Location(location));
     }
 
     public static void compareBoards(ChessBoard firstBoard, ChessBoard secondBoard) {
