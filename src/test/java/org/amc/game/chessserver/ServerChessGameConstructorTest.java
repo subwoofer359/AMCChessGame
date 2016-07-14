be34ea48aa1c9130d1ac5efdd546333a566ad1eb
Adrian McLaughlin <subwoofer359@gmail.com>
2016-07-14 16:46:13 +0100

    remove duplicate code fro /ServerChessGameConstructorTest which is in AbstractServerChessGameTest"

--- src/test/java/org/amc/game/chessserver/ServerChessGameConstructorTest.java
+++ src/test/java/org/amc/game/chessserver/ServerChessGameConstructorTest.java
@@ -4,12 +4,10 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 import org.amc.game.chess.ChessBoard;
-import org.amc.game.chess.ChessBoardUtilities;
 import org.amc.game.chess.ChessGame;
 import org.amc.game.chess.ChessGameFactory;
 import org.amc.game.chess.ChessGameFixture;
 import org.amc.game.chess.ChessGamePlayer;
-import org.amc.game.chess.ComparePlayers;
 import org.amc.game.chess.IllegalMoveException;
 import org.amc.game.chess.Move;
 import org.amc.game.chess.Player;
@@ -82,45 +80,6 @@ public class ServerChessGameConstructorTest {
     public void testNullChessGamePassedToConstructor() {
     	ChessGame c = null;
     	createServerChessGame(INVALID_UID, c);
-    }
-    
-    @Test
-    public void constructorTest() {
-        ServerChessGame scgGame = new ServerChessGame() {
-            
-            private static final long serialVersionUID = 1L;
-            
-            @Override
-            public void addOpponent(Player opponent) {
-                // do nothing
-            }
-        };
-        
-        assertEquals(0L, scgGame.getUid());
-        assertNull(scgGame.getChessGame());
-        assertNull(scgGame.getPlayer());
-        assertNull(scgGame.getOpponent());
-        assertEquals(ServerGameStatus.NEW, scgGame.getCurrentStatus());
-        assertEquals(0, scgGame.getNoOfObservers());
-        assertNull(scgGame.getChessGameFactory());
-    }
-    
-    @Test
-    public void constructorChessGameTest() {
-        ChessGameFixture fixture = new ChessGameFixture();
-        ServerChessGame scgGame = createServerChessGame(UID, fixture.getChessGame());
-        
-        assertEquals(UID, scgGame.getUid());
-        assertNotNull(scgGame.getChessGame());
-        assertFalse(scgGame.getChessGame() == fixture.getChessGame());
-        assertTrue(ComparePlayers.comparePlayers(fixture.getWhitePlayer(), scgGame.getPlayer()));
-        assertTrue(ComparePlayers.comparePlayers(fixture.getBlackPlayer(), scgGame.getOpponent()));
-        assertEquals(ServerGameStatus.IN_PROGRESS, scgGame.getCurrentStatus());
-        assertEquals(0, scgGame.getNoOfObservers());
-        assertNotNull(scgGame.getChessGameFactory());
-        
-        ChessBoardUtilities.compareBoards(fixture.getChessGame().getChessBoard(), 
-                        scgGame.getChessGame().getChessBoard());
     }
     
     @Test
