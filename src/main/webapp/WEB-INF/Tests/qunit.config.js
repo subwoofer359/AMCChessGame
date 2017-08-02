6660d10030c6eabb27cc246ec8fdcc2ea7208953
Adrian McLaughlin <subwoofer359@gmail.com>
2017-08-02 12:06:09 +0100

    add config to run QUnit with requirejs

---
+++ src/main/webapp/WEB-INF/Tests/qunit.config.js
@@ -0,0 +1,8 @@
+requirejs.config({
+    paths: {
+        jquery: "http://code.jquery.com/jquery-2.2.4.min",
+        QUnit: "http://code.jquery.com/qunit/qunit-1.18.0",
+        sinon : "../../js/sinon-1.17.3",
+        snapsvg : "../../js/snap.svg"
+    }
+});
