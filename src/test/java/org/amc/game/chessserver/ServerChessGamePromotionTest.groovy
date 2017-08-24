403574e26ca08c2b99fa024246e65f0df07fa21a
Adrian McLaughlin <subwoofer359@gmail.com>
2017-08-24 15:13:28 +0100

    refactory ServerChessGamePromotionTest

--- src/test/java/org/amc/game/chessserver/ServerChessGamePromotionTest.groovy
+++ src/test/java/org/amc/game/chessserver/ServerChessGamePromotionTest.groovy
@@ -2,12 +2,12 @@ package org.amc.game.chessserver;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
+import static org.amc.game.chess.AbstractChessGame.GameState.*;
 
 import org.amc.game.chess.ChessBoard;
 import org.amc.game.chess.ChessBoardFactory;
 import org.amc.game.chess.ChessBoardFactoryImpl;
 import org.amc.game.chess.ChessGame;
-import org.amc.game.chess.AbstractChessGame.GameState;
 import org.amc.game.chess.ChessGamePlayer;
 import org.amc.game.chess.Colour;
 import org.amc.game.chess.ComparePlayers;
@@ -22,58 +22,64 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 class ServerChessGamePromotionTest {
-    AbstractServerChessGame scGame;
-    Long GAME_UID = 123L;
-    ChessGame chessGame;
-    static ChessGamePlayer playerWhite;
-    static ChessGamePlayer playerBlack;
     
+	private static ChessGamePlayer playerWhite;
+	private static ChessGamePlayer playerBlack;
+	private static final ChessBoardFactory cFactory = new ChessBoardFactoryImpl(new SimpleChessBoardSetupNotation());
+	private static final StandardChessGameFactory sFactory = new StandardChessGameFactory();
+	private static final String CHESSBOARD = 'Ke8:ke1:pa7';
+	private static final Move move = new Move('a7:a8');
+	private static Long GAME_UID = 123L;
+	
+	AbstractServerChessGame scGame;
+    ChessGame chessGame;
     
     @BeforeClass
     public static void setUpClass() throws Exception {
-        playerWhite = new RealChessGamePlayer(new HumanPlayer("playerOne"), Colour.WHITE);
-        playerBlack = new RealChessGamePlayer(new HumanPlayer("playerTwo"), Colour.BLACK);
+        playerWhite = new RealChessGamePlayer(new HumanPlayer('playerOne'), Colour.WHITE);
+        playerBlack = new RealChessGamePlayer(new HumanPlayer('playerTwo'), Colour.BLACK);
     }
     
     @Before
-    public void setUp() throws Exception {
-        ChessBoardFactory cFactory = new ChessBoardFactoryImpl(new SimpleChessBoardSetupNotation());
-        ChessBoard board = cFactory.getChessBoard("Ke8:ke1:pa7");
-        StandardChessGameFactory sFactory = new StandardChessGameFactory();
-        chessGame = sFactory.getChessGame(board, playerWhite, playerBlack);
+    void setUp() {
+        chessGame = sFactory.getChessGame(cFactory.getChessBoard(CHESSBOARD), playerWhite, playerBlack);
         scGame = new TwoViewServerChessGame(GAME_UID, chessGame);
     }
 	
     @Test
-    public void test() {
-        Move move = new Move("a7:a8");
+    void test() {
+        Move move = new Move();
         AbstractServerChessGame spyScGame = spy(scGame);
-        spyScGame.move(playerWhite, move);
-        assert scGame.getChessGame().getGameState() == GameState.PAWN_PROMOTION;
-        verify(spyScGame, times(1)).notifyObservers(GameState.PAWN_PROMOTION);
+        
+		spyScGame.move(playerWhite, move);
+        
+		assert scGame.chessGame.gameState == PAWN_PROMOTION;
+        verify(spyScGame, times(1)).notifyObservers(PAWN_PROMOTION);
         assert ComparePlayers.isSamePlayer(playerWhite, scGame.getChessGame().getCurrentPlayer());
     }
     
-    @Test(expected = IllegalMoveException.class)
-    public void testNoBlackMoveBeforePromotion() {
-        Move move = new Move("a7:a8");
+    @Test(expected = IllegalMoveException)
+    void testNoBlackMoveBeforePromotion() {
         AbstractServerChessGame spyScGame = spy(scGame);
-        spyScGame.move(playerWhite, move);
-        assert scGame.getChessGame().getGameState() == GameState.PAWN_PROMOTION;
-        verify(spyScGame, times(1)).notifyObservers(GameState.PAWN_PROMOTION);
         
-        spyScGame.move(playerBlack, new Move("e8:e7"));
+		spyScGame.move(playerWhite, move);
+        
+		assert scGame.chessGame.gameState == PAWN_PROMOTION;
+        verify(spyScGame, times(1)).notifyObservers(PAWN_PROMOTION);
+        
+        spyScGame.move(playerBlack, new Move('e8:e7'));
     }
     
-    @Test(expected = IllegalMoveException.class)
-    public void testNoWhiteMoveBeforePromotion() {
-        Move move = new Move("a7:a8");
+    @Test(expected = IllegalMoveException)
+    void testNoWhiteMoveBeforePromotion() {
         AbstractServerChessGame spyScGame = spy(scGame);
-        spyScGame.move(playerWhite, move);
-        assert scGame.getChessGame().getGameState() == GameState.PAWN_PROMOTION;
-        verify(spyScGame, times(1)).notifyObservers(GameState.PAWN_PROMOTION);
         
-        spyScGame.move(playerWhite, new Move("e1:e2"));
+		spyScGame.move(playerWhite, move);
+        
+		assert scGame.chessGame.gameState == PAWN_PROMOTION;
+        verify(spyScGame, times(1)).notifyObservers(PAWN_PROMOTION);
+        
+        spyScGame.move(playerWhite, new Move('e1:e2'));
     }
 
 }
